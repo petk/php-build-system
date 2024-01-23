8ed45b603c46a72b2dbc3c58f6a280c5a0252121
Peter Kokot <peterkokot@gmail.com>
2024-01-23 02:46:33 +0100

    Fix macOS build

--- cmake/cmake/platform/Darwin.cmake
+++ cmake/cmake/platform/Darwin.cmake
@@ -4,4 +4,21 @@ Specific configuration for Darwin target (macOS, OS X, etc.).
 
 include_guard(GLOBAL)
 
+include(CheckLinkerFlag)
+
 set(DARWIN 1 CACHE INTERNAL "Define if the target system is Darwin")
+
+# TODO: This is still needed for shared extensions on macOS, otherwise undefined
+# symbol errors happen in the linking step when using Clang.
+check_linker_flag(
+  C
+  "LINKER:-undefined,dynamic_lookup"
+  HAVE_UNDEFINED_DYNAMIC_LOOKUP_FLAG_C
+)
+if(HAVE_UNDEFINED_DYNAMIC_LOOKUP_FLAG_C)
+  target_link_options(
+    php_configuration
+    INTERFACE
+      $<$<IN_LIST:$<TARGET_PROPERTY:TYPE>,MODULE_LIBRARY;SHARED_LIBRARY>:LINKER:-undefined,dynamic_lookup>
+  )
+endif()
