adce496727e923d18b392a0b990d047a8bed2873
Peter Kokot <peterkokot@gmail.com>
2024-01-27 14:07:58 +0100

    Sync pspell and imap extensions with updated find modules

--- cmake/cmake/modules/FindAspell.cmake
+++ cmake/cmake/modules/FindAspell.cmake
@@ -6,22 +6,28 @@ Aspell library. The Aspell library provides a simple pspell interface (pspell.h)
 for backwards compatibility. On some systems there is a package libpspell-dev,
 however relying on it is not encouraged.
 
-Module defines the following IMPORTED targets:
+Module defines the following IMPORTED target(s):
 
   Aspell::Aspell
-    The Aspell library, if found.
+    The package library, if found.
 
 Result variables:
 
   Aspell_FOUND
-    Whether Aspell has been found.
+    Whether the package has been found.
   Aspell_INCLUDE_DIRS
-    A list of include directories for using Aspell library.
-  Aspell_PSPELL_INCLUDE_DIRS
-    A list of include directories for using Aspell library with pspell BC
-    interface.
+    Include directories needed to use this package.
   Aspell_LIBRARIES
-    A list of libraries for linking when using Aspell library.
+    Libraries needed to link to the package library.
+
+Cache variables:
+
+  Aspell_INCLUDE_DIR
+    Directory containing package library headers.
+  Aspell_LIBRARY
+    The path to the package library.
+  Aspell_PSPELL_INCLUDE_DIR
+    Directory containing the pspell.h BC interface header if available.
 
 Hints:
 
@@ -32,63 +38,84 @@ include(CheckLibraryExists)
 include(FeatureSummary)
 include(FindPackageHandleStandardArgs)
 
-set_package_properties(Aspell PROPERTIES
-  URL "http://aspell.net/"
-  DESCRIPTION "GNU Aspell spell checker library"
+set_package_properties(
+  Aspell
+  PROPERTIES
+    URL "http://aspell.net/"
+    DESCRIPTION "GNU Aspell spell checker library"
 )
 
-set(_reason_failure_message)
+set(_reason "")
 
-find_path(Aspell_INCLUDE_DIRS aspell.h)
+find_path(
+  Aspell_INCLUDE_DIR
+  NAMES aspell.h
+  DOC "Directory containing Aspell library headers"
+)
 
-if(NOT Aspell_INCLUDE_DIRS)
-  string(
-    APPEND _reason_failure_message
-    "\n    The aspell.h could not be found."
-  )
+if(NOT Aspell_INCLUDE_DIR)
+  string(APPEND _reason "aspell.h could not be found. ")
 endif()
 
 # If there is also pspell interface.
-find_path(Aspell_PSPELL_INCLUDE_DIRS pspell.h PATH_SUFFIXES pspell)
+find_path(
+  Aspell_PSPELL_INCLUDE_DIR
+  NAMES pspell.h
+  PATH_SUFFIXES pspell
+  DOC "Directory containing pspell.h BC interface header"
+)
 
-if(Aspell_INCLUDE_DIRS AND Aspell_PSPELL_INCLUDE_DIRS)
-  list(APPEND Aspell_INCLUDE_DIRS ${Aspell_PSPELL_INCLUDE_DIRS})
-endif()
+find_library(
+  Aspell_LIBRARY
+  NAMES aspell
+  DOC "The path to the Aspell library"
+)
 
-find_library(Aspell_LIBRARIES NAMES aspell DOC "The Aspell library")
-
-if(NOT Aspell_LIBRARIES)
-  string(
-    APPEND _reason_failure_message
-    "\n    Aspell not found. Please install the Aspell library."
-  )
+if(NOT Aspell_LIBRARY)
+  string(APPEND _reason "Aspell library not found. ")
 endif()
 
 # Sanity check.
-if(Aspell_LIBRARIES)
+if(Aspell_LIBRARY)
   check_library_exists(
-    "${Aspell_LIBRARIES}"
+    "${Aspell_LIBRARY}"
     new_aspell_config
     ""
-    _aspell_have_new_aspell_config
+    _aspell_sanity_check
   )
+
+  if(NOT _aspell_sanity_check)
+    string(APPEND _reason "Sanity check failed: new_aspell_config not found. ")
+  endif()
 endif()
 
-mark_as_advanced(Aspell_LIBRARIES Aspell_INCLUDE_DIRS)
+mark_as_advanced(Aspell_INCLUDE_DIR Aspell_PSPELL_INCLUDE_DIR Aspell_LIBRARY)
 
 find_package_handle_standard_args(
   Aspell
-  REQUIRED_VARS Aspell_LIBRARIES Aspell_INCLUDE_DIRS _aspell_have_new_aspell_config
-  REASON_FAILURE_MESSAGE "${reason_failure_message}"
+  REQUIRED_VARS
+    Aspell_LIBRARY
+    Aspell_INCLUDE_DIR
+    _aspell_sanity_check
+  REASON_FAILURE_MESSAGE "${_reason}"
 )
 
-unset(_reason_failure_message)
+unset(_reason)
 
-if(Aspell_FOUND AND NOT TARGET Aspell::Aspell)
-  add_library(Aspell::Aspell INTERFACE IMPORTED)
+if(NOT Aspell_FOUND)
+  return()
+endif()
 
-  set_target_properties(Aspell::Aspell PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${Aspell_INCLUDE_DIRS}"
-    INTERFACE_LINK_LIBRARIES "${Aspell_LIBRARIES}"
+set(Aspell_INCLUDE_DIRS ${Aspell_INCLUDE_DIR} ${Aspell_PSPELL_INCLUDE_DIR})
+set(Aspell_LIBRARIES ${Aspell_LIBRARY})
+
+if(NOT TARGET Aspell::Aspell)
+  add_library(Aspell::Aspell UNKNOWN IMPORTED)
+
+  set_target_properties(
+    Aspell::Aspell
+    PROPERTIES
+      IMPORTED_LOCATION "${Aspell_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${Aspell_INCLUDE_DIR};${Aspell_PSPELL_INCLUDE_DIR}"
   )
 endif()
---
+++ cmake/cmake/modules/FindCclient.cmake
@@ -0,0 +1,175 @@
+#[=============================================================================[
+Find the IMAP c-client library.
+
+Also called UW-IMAP library was once maintained by the Washington University.
+Today it is obsolete and its usage is discouraged. The c-client is a component
+of the IMAP library that can be found as a standalone package on systems.
+
+Module defines the following IMPORTED target(s):
+
+  Cclient::Cclient
+    The package library, if found.
+
+Result variables:
+
+  Cclient_FOUND
+    Whether the package has been found.
+  Cclient_INCLUDE_DIRS
+    Include directories needed to use this package.
+  Cclient_LIBRARIES
+    Libraries needed to link to the package library.
+
+Cache variables:
+
+  Cclient_INCLUDE_DIR
+    Directory containing package library headers.
+  Cclient_LIBRARY
+    The path to the package library.
+
+Hints:
+
+  The Cclient_ROOT variable adds custom search path.
+
+The UW-IMAP c-client library was not originally designed to be a shared library.
+The mm_<name> functions are callbacks, and are required to be implemented by the
+program that is linking to c-client. Therefore this module also exposes function
+cclient_check_symbol(), which does the work of defining them all to no-ops for
+you. Note that this is a link test. The undefined symbols will only cause
+problems if you actually try to link with c-client. For example, if your test is
+trivial enough to be optimized out, and if you link with --as-needed, the
+test/library may be omitted entirely from the final executable. In that case
+linking will of course succeed, but your luck won't necessarily apply at lower
+optimization levels or systems where --as-needed is not used.
+
+  cclient_check_symbol(<symbol> <result>)
+
+    <symbol>
+      Symbol name to check if it is available in the c-client.
+
+    <result>
+      Cache variable name for storing the check result.
+#]=============================================================================]
+
+include(CheckSourceCompiles)
+include(CMakePushCheckState)
+include(FeatureSummary)
+include(FindPackageHandleStandardArgs)
+
+function(cclient_check_symbol symbol result)
+  message(CHECK_START "Looking for ${symbol}")
+
+  cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_LIBRARIES ${Cclient_LIBRARY})
+    set(CMAKE_REQUIRED_QUIET TRUE)
+
+    check_source_compiles(C "
+      #if defined(__GNUC__) && __GNUC__ >= 4
+      # define IMAP_CCLIENT_EXPORT __attribute__ ((visibility(\"default\")))
+      #else
+      # define IMAP_CCLIENT_EXPORT
+      #endif
+
+      IMAP_CCLIENT_EXPORT void mm_critical(void){}
+      IMAP_CCLIENT_EXPORT void mm_diskerror(void){}
+      IMAP_CCLIENT_EXPORT void mm_dlog(void){}
+      IMAP_CCLIENT_EXPORT void mm_exists(void){}
+      IMAP_CCLIENT_EXPORT void mm_expunged(void){}
+      IMAP_CCLIENT_EXPORT void mm_fatal(void){}
+      IMAP_CCLIENT_EXPORT void mm_flags(void){}
+      IMAP_CCLIENT_EXPORT void mm_list(void){}
+      IMAP_CCLIENT_EXPORT void mm_log(void){}
+      IMAP_CCLIENT_EXPORT void mm_login(void){}
+      IMAP_CCLIENT_EXPORT void mm_lsub(void){}
+      IMAP_CCLIENT_EXPORT void mm_nocritical(void){}
+      IMAP_CCLIENT_EXPORT void mm_notify(void){}
+      IMAP_CCLIENT_EXPORT void mm_searched(void){}
+      IMAP_CCLIENT_EXPORT void mm_status(void){}
+
+      void ${symbol}(void);
+      void (*f)(void);
+      char foobar() {
+        f = ${symbol};
+      }
+
+      int main(void) {
+          foobar();
+
+          return 0;
+      }
+    " ${result})
+  cmake_pop_check_state()
+
+  if(${${result}})
+    message(CHECK_PASS "found")
+  else()
+    message(CHECK_PASS "not found")
+  endif()
+endfunction()
+
+set_package_properties(
+  Cclient
+  PROPERTIES
+    URL "https://en.wikipedia.org/wiki/UW_IMAP"
+    DESCRIPTION "The IMAP c-client library"
+)
+
+set(_reason "")
+
+find_path(
+  Cclient_INCLUDE_DIR
+  NAMES c-client.h rfc822.h
+  PATH_SUFFIXES c-client imap
+  DOC "Directory containing c-client library headers"
+)
+
+if(NOT Cclient_INCLUDE_DIR)
+  string(APPEND _reason "c-client.h or rfc822.h not found. ")
+endif()
+
+find_library(
+  Cclient_LIBRARY
+  NAMES c-client c-client4 imap
+  DOC "The path to the c-client library"
+)
+
+if(NOT Cclient_LIBRARY)
+  string(APPEND _reason "IMAP c-client library not found. ")
+endif()
+
+mark_as_advanced(Cclient_INCLUDE_DIR Cclient_LIBRARY)
+
+# Sanity check.
+cclient_check_symbol(mail_newbody _cclient_sanity_check)
+
+if(NOT _cclient_sanity_check)
+  string(APPEND _reason "Sanity check failed: mail_newbody() not found. ")
+endif()
+
+find_package_handle_standard_args(
+  Cclient
+  REQUIRED_VARS
+    Cclient_LIBRARY
+    Cclient_INCLUDE_DIR
+    _cclient_sanity_check
+  REASON_FAILURE_MESSAGE "${_reason}"
+)
+
+unset(_reason)
+
+if(NOT Cclient_FOUND)
+  return()
+endif()
+
+set(Cclient_INCLUDE_DIRS ${Cclient_INCLUDE_DIR})
+set(Cclient_LIBRARIES ${Cclient_LIBRARY})
+
+if(NOT TARGET Cclient::Cclient)
+  add_library(Cclient::Cclient UNKNOWN IMPORTED)
+
+  set_target_properties(
+    Cclient::Cclient
+    PROPERTIES
+      IMPORTED_LOCATION "${Cclient_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${Cclient_INCLUDE_DIR}"
+  )
+endif()
