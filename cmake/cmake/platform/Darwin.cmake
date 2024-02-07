72d4eb69d899f65e313e7366e9943460eca14f68
Peter Kokot <peterkokot@gmail.com>
2024-02-07 21:09:34 +0100

    Remove redundant DARWIN PHP custom symbol

--- cmake/cmake/platform/Darwin.cmake
+++ cmake/cmake/platform/Darwin.cmake
@@ -6,8 +6,6 @@ include_guard(GLOBAL)
 
 include(CheckLinkerFlag)
 
-set(DARWIN 1 CACHE INTERNAL "Define if the target system is Darwin")
-
 # TODO: Fix this properly and add it to required definitions.
 set(_DARWIN_C_SOURCE 1 CACHE INTERNAL "")
 
