d7a3ccfdefaa9ba89c98c33f7982aa01984eef23
Peter Kokot <peterkokot@gmail.com>
2024-05-05 23:55:21 +0200

    Add interprocedural optimization (IPO)
    IPO adds linker flags if they are supported by the compiler to run
    link-time optimizations on targets.

---
+++ cmake/cmake/Bootstrap.cmake
@@ -0,0 +1,18 @@
+#[=============================================================================[
+Configure project after the project() call.
+]=============================================================================]#
+
+include_guard(GLOBAL)
+
+# Output linker information.
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.29)
+  if(CMAKE_C_COMPILER_LINKER)
+    message(STATUS "C linker: ${CMAKE_C_COMPILER_LINKER}")
+  endif()
+  if(CMAKE_CXX_COMPILER_LINKER)
+    message(STATUS "CXX linker: ${CMAKE_CXX_COMPILER_LINKER}")
+  endif()
+endif()
+
+# Check whether to enable interprocedural optimization.
+include(PHP/InterproceduralOptimization)
