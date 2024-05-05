79a829c85cccf836dcb49d588cd714310d717597
Peter Kokot <peterkokot@gmail.com>
2024-05-06 00:03:45 +0200

    Fix typo

--- cmake/cmake/modules/PHP/InterproceduralOptimization.cmake
+++ cmake/cmake/modules/PHP/InterproceduralOptimization.cmake
@@ -4,7 +4,7 @@ Enable interprocedural optimization (IPO), if supported.
 Interprocedural optimization adds linker flag (-flto) if it is supported by the
 compiler to run standard link-time optimizer.
 
-It can be also controled more granular by the user with the
+It can be also controlled more granular by the user with the
 CMAKE_INTERPROCEDURAL_OPTIMIZATION_<CONFIG> variables based on the build type.
 
 https://cmake.org/cmake/help/latest/prop_tgt/INTERPROCEDURAL_OPTIMIZATION.html
