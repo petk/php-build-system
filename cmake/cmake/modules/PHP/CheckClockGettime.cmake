7523affc1ab823a1426cfbe9c1994091bab3e1fd
Peter Kokot <peterkokot@gmail.com>
2024-06-02 22:45:00 +0200

    Sync with upstream

--- cmake/cmake/modules/PHP/CheckClockGettime.cmake
+++ cmake/cmake/modules/PHP/CheckClockGettime.cmake
@@ -47,15 +47,16 @@ if(NOT CMAKE_CROSSCOMPILING)
   cmake_push_check_state(RESET)
     set(CMAKE_REQUIRED_QUIET TRUE)
 
-    check_source_runs(C "
+    check_source_runs(C [[
       #include <mach/mach.h>
       #include <mach/clock.h>
       #include <mach/mach_error.h>
 
-      int main(void) {
+      int main(void)
+      {
         kern_return_t ret; clock_serv_t aClock; mach_timespec_t aTime;
-        ret = host_get_clock_service(mach_host_self(), REALTIME_CLOCK, &aClock);
 
+        ret = host_get_clock_service(mach_host_self(), REALTIME_CLOCK, &aClock);
         if (ret != KERN_SUCCESS) {
           return 1;
         }
@@ -67,7 +68,7 @@ if(NOT CMAKE_CROSSCOMPILING)
 
         return 0;
       }
-    " HAVE_CLOCK_GET_TIME)
+    ]] HAVE_CLOCK_GET_TIME)
   cmake_pop_check_state()
 endif()
 
