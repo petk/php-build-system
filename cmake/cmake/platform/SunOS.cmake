d79ab3c6435ebdd17877a173e7dc2e64f5450e41
Peter Kokot <peterkokot@gmail.com>
2024-08-22 20:00:09 +0200

    Add warning for Solaris 10

--- cmake/cmake/platform/SunOS.cmake
+++ cmake/cmake/platform/SunOS.cmake
@@ -5,6 +5,17 @@ Specific configuration for SunOS patform (Solaris, illumos, etc.).
 include_guard(GLOBAL)
 
 if(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
+  # Issue a warning for Solaris 10 system on this build system due to too
+  # possible compilation warnings and issues with outdated.
+  if(CMAKE_SYSTEM_VERSION VERSION_LESS 5.11)
+    message(
+      WARNING
+      "Solaris 10 with support from 2005 to 2027 might have a limited "
+      "functionality. Check if you can upgrade to Solaris 11 or later, or try "
+      "one of the illumos-based distributions."
+    )
+  endif()
+
   target_compile_definitions(
     php_configuration
     INTERFACE
