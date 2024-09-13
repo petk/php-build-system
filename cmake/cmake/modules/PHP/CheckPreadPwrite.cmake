5ef0daf1579d2a797bcad3738dbe86a7b648ad1b
Peter Kokot <peterkokot@gmail.com>
2024-09-13 12:32:18 +0200

    Improve cross-compiling for more run checks

--- cmake/cmake/modules/PHP/CheckPreadPwrite.cmake
+++ cmake/cmake/modules/PHP/CheckPreadPwrite.cmake
@@ -45,51 +45,59 @@ function(_php_check_pread)
     return()
   endif()
 
-  if(NOT CMAKE_CROSSCOMPILING)
-    set(
-      temporaryFile
-      ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pread.tmp
-    )
+  set(
+    temporaryFile
+    ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pread.tmp
+  )
 
-    file(WRITE "${temporaryFile}" "test\n")
+  file(WRITE "${temporaryFile}" "test\n")
 
-    cmake_push_check_state(RESET)
-      set(CMAKE_REQUIRED_QUIET TRUE)
-      check_source_runs(C "
-        #include <sys/types.h>
-        #include <sys/stat.h>
-        #include <fcntl.h>
-        #include <unistd.h>
-        #include <errno.h>
-        #include <stdlib.h>
+  cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_QUIET TRUE)
+    set(CMAKE_REQUIRED_DEFINITIONS -DTMP_FILE=${temporaryFile})
 
-        int main(void) {
-          char buf[3];
-          int fd = open(\"${temporaryFile}\", O_RDONLY);
+    check_source_runs(C [[
+      #define xstr(s) str(s)
+      #define str(s) #s
 
-          if (fd < 0) return 1;
-          if (pread(fd, buf, 2, 0) != 2) return 1;
-          /* Linux glibc breakage until 2.2.5 */
-          if (pread(fd, buf, 2, -1) != -1 || errno != EINVAL) return 1;
+      #include <sys/types.h>
+      #include <sys/stat.h>
+      #include <fcntl.h>
+      #include <unistd.h>
+      #include <errno.h>
+      #include <stdlib.h>
 
-          return 0;
-        }
-      " HAVE_PREAD)
-    cmake_pop_check_state()
-  endif()
+      int main(void)
+      {
+        char buf[3];
+        int fd = open(xstr(TMP_FILE), O_RDONLY);
+
+        if (fd < 0) return 1;
+        if (pread(fd, buf, 2, 0) != 2) return 1;
+        /* Linux glibc breakage until 2.2.5 */
+        if (pread(fd, buf, 2, -1) != -1 || errno != EINVAL) return 1;
+
+        return 0;
+      }
+    ]] HAVE_PREAD)
+  cmake_pop_check_state()
 
   # This check is obsolete. Some systems once had pread() available with 3rd
   # argument of type 'off64_t', but didn't provide declaration in the headers.
-  if(NOT HAVE_PREAD AND NOT CMAKE_CROSSCOMPILING)
+  if(NOT HAVE_PREAD)
     cmake_push_check_state(RESET)
       set(CMAKE_REQUIRED_QUIET TRUE)
+      set(CMAKE_REQUIRED_DEFINITIONS -DTMP_FILE=${temporaryFile})
 
       # Needs '_GNU_SOURCE' to enable '_LARGEFILE64_SOURCE' for using 'off64_t'.
       # Default way would be the '_FILE_OFFSET_BITS=64' but this is skipped to
       # match the current php-src code.
       set(CMAKE_REQUIRED_LIBRARIES PHP::SystemExtensions)
 
-      check_source_runs(C "
+      check_source_runs(C [[
+        #define xstr(s) str(s)
+        #define str(s) #s
+
         #include <sys/types.h>
         #include <sys/stat.h>
         #include <fcntl.h>
@@ -100,9 +108,10 @@ function(_php_check_pread)
         /* Provide a missing declaration. */
         ssize_t pread(int, void *, size_t, off64_t);
 
-        int main(void) {
+        int main(void)
+        {
           char buf[3];
-          int fd = open(\"${temporaryFile}\", O_RDONLY);
+          int fd = open(xstr(TMP_FILE), O_RDONLY);
 
           if (fd < 0) return 1;
           if (pread(fd, buf, 2, 0) != 2) return 1;
@@ -111,7 +120,7 @@ function(_php_check_pread)
 
           return 0;
         }
-      " PHP_PREAD_64)
+      ]] PHP_PREAD_64)
     cmake_pop_check_state()
 
     if(PHP_PREAD_64)
@@ -144,56 +153,59 @@ function(_php_check_pwrite)
     return()
   endif()
 
-  if(NOT CMAKE_CROSSCOMPILING)
+  cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_QUIET TRUE)
     set(
-      temporaryFile
-      ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pwrite.tmp
+      CMAKE_REQUIRED_DEFINITIONS
+      -DTMP_FILE=${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pwrite.tmp
     )
 
-    cmake_push_check_state(RESET)
-      set(CMAKE_REQUIRED_QUIET TRUE)
+    check_source_runs(C [[
+      #define xstr(s) str(s)
+      #define str(s) #s
 
-      check_source_runs(C "
-        #include <sys/types.h>
-        #include <sys/stat.h>
-        #include <fcntl.h>
-        #include <unistd.h>
-        #include <errno.h>
-        #include <stdlib.h>
+      #include <sys/types.h>
+      #include <sys/stat.h>
+      #include <fcntl.h>
+      #include <unistd.h>
+      #include <errno.h>
+      #include <stdlib.h>
 
-        int main(void) {
-          int fd = open(\"${temporaryFile}\", O_WRONLY|O_CREAT, 0600);
+      int main(void)
+      {
+        int fd = open(xstr(TMP_FILE), O_WRONLY|O_CREAT, 0600);
 
-          if (fd < 0) return 1;
-          if (pwrite(fd, \"text\", 4, 0) != 4) return 1;
-          /* Linux glibc breakage until 2.2.5 */
-          if (pwrite(fd, \"text\", 4, -1) != -1 || errno != EINVAL) return 1;
+        if (fd < 0) return 1;
+        if (pwrite(fd, "text", 4, 0) != 4) return 1;
+        /* Linux glibc breakage until 2.2.5 */
+        if (pwrite(fd, "text", 4, -1) != -1 || errno != EINVAL) return 1;
 
-          return 0;
-        }
-      " HAVE_PWRITE)
-    cmake_pop_check_state()
-  endif()
+        return 0;
+      }
+    ]] HAVE_PWRITE)
+  cmake_pop_check_state()
 
   # This check is obsolete. Some systems once had pwrite() available with 3rd
   # argument of type 'off64_t', but didn't provide declaration in the headers.
   # On later systems the 2nd argument of pwrite() should also have the 'const'
   # keyword.
-  if(NOT HAVE_PWRITE AND NOT CMAKE_CROSSCOMPILING)
-    set(
-      temporaryFile
-      ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pwrite64.tmp
-    )
-
+  if(NOT HAVE_PWRITE)
     cmake_push_check_state(RESET)
       set(CMAKE_REQUIRED_QUIET TRUE)
+      set(
+        CMAKE_REQUIRED_DEFINITIONS
+        -DTMP_FILE=${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/php_check_pwrite64.tmp
+      )
 
       # Needs '_GNU_SOURCE' to enable '_LARGEFILE64_SOURCE' for using 'off64_t'.
       # Default way would be the '_FILE_OFFSET_BITS=64' but this is skipped to
       # match the current php-src code.
       set(CMAKE_REQUIRED_LIBRARIES PHP::SystemExtensions)
 
-      check_source_runs(C "
+      check_source_runs(C [[
+        #define xstr(s) str(s)
+        #define str(s) #s
+
         #include <sys/types.h>
         #include <sys/stat.h>
         #include <fcntl.h>
@@ -204,17 +216,18 @@ function(_php_check_pwrite)
         /* Provide a missing declaration. */
         ssize_t pwrite(int, void *, size_t, off64_t);
 
-        int main(void) {
-          int fd = open(\"${temporaryFile}\", O_WRONLY|O_CREAT, 0600);
+        int main(void)
+        {
+          int fd = open(xstr(TMP_FILE), O_WRONLY|O_CREAT, 0600);
 
           if (fd < 0) return 1;
-          if (pwrite(fd, \"text\", 4, 0) != 4) return 1;
+          if (pwrite(fd, "text", 4, 0) != 4) return 1;
           /* Linux glibc breakage until 2.2.5 */
-          if (pwrite(fd, \"text\", 4, -1) != -1 || errno != EINVAL) return 1;
+          if (pwrite(fd, "text", 4, -1) != -1 || errno != EINVAL) return 1;
 
           return 0;
         }
-      " PHP_PWRITE_64)
+      ]] PHP_PWRITE_64)
     cmake_pop_check_state()
 
     if(PHP_PWRITE_64)
--- cmake/cmake/toolchains/template.cmake
+++ cmake/cmake/toolchains/template.cmake
@@ -2,39 +2,39 @@
 # cross-compiling to help identify the target system when cross-compiling
 # emulator is not available.
 
-# Set to 0 if GNU C compiler has broken strlen, and 1 if not.
+# Set the exit code for the GNU C compiler with broken strlen check.
 set(HAVE_BROKEN_OPTIMIZE_STRLEN_EXITCODE 1)
 
-# Set to 0 if target system has clock_get_time(), and to 1 if not.
+# Set the exit code for the clock_get_time() check.
 set(HAVE_CLOCK_GET_TIME_EXITCODE 0)
 
-# Set to 0 if flush should be called explicitly after a buffered io, and to 1 if
-# not.
+# Set the exit code if flush should be called explicitly after a buffered io.
 set(HAVE_FLUSHIO_EXITCODE 1)
 
-# Set to 0 if target system has POSIX fnmatch() and to 0 if not.
+# Set the exit code for the POSIX fnmatch() check.
 set(HAVE_FNMATCH_EXITCODE 0)
 
-# Set to 0 if curl library is linked against OpenSSL 1.1 or earlier, and to 1 if
-# or greater if it is linked against newer OpenSSL.
+# Set the exit code for the check whether the curl library is linked with
+# OpenSSL 1.1 or earlier.
 set(HAVE_CURL_OLD_OPENSSL_EXITCODE 4)
 
-# Set to 0 if the bundled PCRE library can have JIT supported on the target
-# architecture, and to 1 if not.
+# Set the exit code for the check whether the bundled PCRE library has JIT
+# supported on the target architecture.
 set(HAVE_PCRE_JIT_SUPPORT_EXITCODE 0)
 
-# Set to 0 if fopencookie seeker uses off64_t, and to 1 if regular off_t type
-# can be used.
+# Set the exit code for the fopencookie seeker using off64_t check.
 set(COOKIE_SEEKER_USES_OFF64_T_EXITCODE 0)
 
-# Set to 0 if reentrant time-related functions are HP-UX style, and to 1 if not.
+# Set the exit code for the check of reentrant time-related functions being HP-UX
+# style.
 set(PHP_HPUX_TIME_R_EXITCODE 1)
 
-# Set to 0 if reentrant time-related functions are IRIX style, and to 1 if not.
+# Set the exit code for the check of reentrant time-related functions being IRIX
+# style.
 set(PHP_IRIX_TIME_R_EXITCODE 1)
 
-# Set the exit code whether the external gd library has support for the given
-# format.
+# Set the exit code for the check whether the external gd library has support
+# for the given format.
 set(HAVE_GD_PNG_EXITCODE 1)
 set(HAVE_GD_AVIF_EXITCODE 1)
 set(HAVE_GD_WEBP_EXITCODE 1)
@@ -43,20 +43,47 @@ set(HAVE_GD_XPM_EXITCODE 1)
 set(HAVE_GD_BMP_EXITCODE 1)
 set(HAVE_GD_TGA_EXITCODE 1)
 
-# Set to 0 if syscall to create shadow stack exists, and to 1 if it doesn't.
+# Set the exit code for the check if syscall to create shadow stack exists.
 set(SHADOW_STACK_SYSCALL_EXITCODE 1)
 
-# Set to exit code for the getaddrinfo() check.
+# Set the exit code for the getaddrinfo() check.
 set(HAVE_GETADDRINFO_EXITCODE 0)
 
-# Set to exit code for the writing to stdout check.
+# Set the exit code for the writing to stdout check.
 set(PHP_WRITE_STDOUT_EXITCODE 0)
+
+# Set the exit code of the stack limit check.
+set(ZEND_CHECK_STACK_LIMIT_EXITCODE 1)
+
+# Set the exit code of the ttyname_r check.
+set(HAVE_TTYNAME_R_EXITCODE 0)
+
+# Set the exit code of the pread check.
+set(HAVE_PREAD_EXITCODE 0)
+set(PHP_PREAD_64_EXITCODE 0)
+
+# Set the exit code of the pwrite check.
+set(HAVE_PWRITE_EXITCODE 0)
+set(PHP_PWRITE_64_EXITCODE 0)
+
+# Set the exit code of the iconv //IGNORE check.
+set(ICONV_BROKEN_IGNORE_EXITCODE 1)
+
+# Set the exit code of the iconv errno check.
+set(PHP_ICONV_ERRNO_WORKS_EXITCODE 0)
+
+# Set the exit code of the ptrace check for PHP FPM.
+set(HAVE_PTRACE_EXITCODE 0)
+
+# Set the exit code of SysV IPC shared memory check in opcache extension.
+set(HAVE_SHM_IPC_EXITCODE 0)
+
+# Set the exit code of the mmap() using MAP_ANON shared memory check in opcache
+# extension.
+set(HAVE_SHM_MMAP_ANON_EXITCODE 0)
+
+# Set the exit code of the shm_open() shared memory check in opcache extension.
+set(HAVE_SHM_MMAP_POSIX_EXITCODE 0)
 
 # TODO: Fix this better.
 set(ZEND_MM_OUTPUT "(size_t)8 (size_t)3 0")
-
-# Set to exit code of the stack limit check.
-set(ZEND_CHECK_STACK_LIMIT_EXITCODE 1)
-
-# Set to exit code of the ttyname_r check.
-set(HAVE_TTYNAME_R_EXITCODE 0)
