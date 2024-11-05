2db71a1d14542e4704dbf8b0f28d120be0c37ce4
Peter Kokot <peterkokot@gmail.com>
2024-11-06 00:39:20 +0100

    Add various improvements
    - Refactor patches and add a new helper script to update patch files
      in the local clone instead of a separate Git repository
    - Fix ndbm and dbm finder modules further with checking whether the
      default libraries (C) has the requested package
    - Fix failing dba tests for ndbm and dbm handlers via patch
    - Refactored the code checker script a bit to not require passing a path
      as it is not so clear what this path should be

---
+++ bin/update-patches.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+#
+# Script for updating php-src patches.
+
+if test -z "$REPO"; then
+  REPO="php-src"
+fi
+
+# A list of supported PHP versions.
+phpVersions="8.3 8.4 8.5"
+
+# The PHP MAJOR.MINOR version currently in development (the master branch).
+phpVersionDev="8.5"
+
+# Temporary Git branch for applying patches and rebasing the branch.
+temporaryBranch=cmake-patching
+
+# Go to project root.
+cd $(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd -P)
+
+while test $# -gt 0; do
+  if test "$1" = "-h" || test "$1" = "--help"; then
+    cat << HELP
+Update php-src patches
+
+A simple helper that updates patches needed to use CMake with php-src source
+code.
+
+SYNOPSIS:
+   $0 [<options>]
+
+OPTIONS:
+  -h, --help       Display this help and exit.
+
+ENVIRONMENT VARIABLES:
+   The following optional variables are supported:
+
+   REPO    Overrides the location to the locally cloned php-src repository.
+           REPO=php-src-repo $0
+HELP
+    exit 0
+  fi
+
+  shift
+done
+
+# Check requirements.
+which=$(which which 2>/dev/null)
+git=$(which git 2>/dev/null)
+
+if test -z "$which"; then
+  echo "update-patches.sh: The 'which' command not found." >&2
+  echo "                   Please install coreutils." >&2
+  exit 1
+fi
+
+if test -z "$git"; then
+  echo "update-patches.sh: The 'git' command not found." >&2
+  echo "                   Please install Git:" >&2
+  echo "                   https://git-scm.com" >&2
+  exit 1
+fi
+
+# Clone a fresh latest php-src repository.
+if test ! -d "${REPO}"; then
+  echo "To use this tool you need php-src Git repository."
+  printf "Do you want to clone it now (y/N)?"
+  read answer
+
+  if test "$answer" != "${answer#[Yy]}"; then
+    echo "Cloning github.com/php/php-src. This will take a little while."
+    "$git" clone https://github.com/php/php-src ${REPO}
+  else
+    exit 1
+  fi
+fi
+
+# Make sure we're in the php-src repository.
+cd ${REPO}
+if test ! -f "main/php_version.h" \
+  || test ! -f "php.ini-development"
+then
+  echo "Git repository doesn't seem to be php-src." >&2
+  exit 1
+fi
+
+# Check if there is existing temporary Git branch to not override.
+if test -n "$(git show-ref refs/heads/${temporaryBranch})"; then
+  echo "E: There is existing ${temporaryBranch} branch." >&2
+  echo "E: Remove or rename ${temporaryBranch} branch." >&2
+  exit 1
+fi
+
+for php in $phpVersions; do
+  if test "$php" = "$phpVersionDev"; then
+    branch=master
+  else
+    branch=PHP-$php
+  fi
+
+  # Check if php-src branch exists.
+  if test -z "$(git show-ref refs/heads/${branch})"; then
+    echo "E: Branch ${branch} is missing." >&2
+    exit 1
+  fi
+
+  "$git" reset --hard
+  "$git" clean -dffx
+  "$git" checkout ${branch}
+  "$git" pull --rebase
+
+  for patch in "$PWD/../patches/$php"/*.patch; do
+    fileName=patches/$php/$(basename "$patch")
+    echo "-> Updating $fileName"
+
+    "$git" checkout -b ${temporaryBranch}
+    "$git" am -3 $patch
+
+    if test "x$?" != "x0"; then
+      echo "E: Patch ${patch} needs manual resolution." >&2
+      echo "   Go to php-src and resolve conflicts manually." >&2
+      exit 1
+    fi
+
+    "$git" --no-pager format-patch -1 ${temporaryBranch} \
+      --stdout \
+      --no-signature \
+      --subject-prefix="" \
+    > ${patch}
+
+    # Remove redundant patch header information.
+    sed -i '/^From /d' ${patch}
+    sed -i '/^Date: /d' ${patch}
+
+    # Replace email if needed.
+    #sed -i 's/^From: .*/From: ElePHPant <elephpant@example.com>/' ${patch}
+
+    "$git" checkout ${branch}
+    "$git" branch -D ${temporaryBranch}
+  done
+done
--- cmake/cmake/modules/FindDbm.cmake
+++ cmake/cmake/modules/FindDbm.cmake
@@ -4,6 +4,14 @@ Find the dbm library.
 Depending on the system, the dbm library can be part of other libraries as an
 interface.
 
+* GNU dbm has compatibility interface via gdbm_compatibility
+* TODO: Built into default libraries (C): Solaris still has some macros
+  definitions mapping to internal dbm functions available in the db.h header.
+  When defining `DB_DBM_HSEARCH` dbm handler is available as built into C
+  library. However, this is museum code and probably relying on a standalone dbm
+  package instead should be done without using this artefact. PHP in the past
+  already used this and moved the db extension out of the php-src to PECL.
+
 Module defines the following `IMPORTED` target(s):
 
 * `Dbm::Dbm` - The package library, if found.
@@ -11,6 +19,7 @@ Module defines the following `IMPORTED` target(s):
 ## Result variables
 
 * `Dbm_FOUND` - Whether the package has been found.
+* `Dbm_IS_BUILT_IN` - Whether dbm is a part of the C library.
 * `Dbm_INCLUDE_DIRS` - Include directories needed to use this package.
 * `Dbm_LIBRARIES` - Libraries needed to link to the package library.
 * `Dbm_IMPLEMENTATION` - String of the library name that implements the dbm
@@ -27,6 +36,8 @@ Module defines the following `IMPORTED` target(s):
 #]=============================================================================]
 
 include(CheckLibraryExists)
+include(CheckSymbolExists)
+include(CMakePushCheckState)
 include(FeatureSummary)
 include(FindPackageHandleStandardArgs)
 
@@ -39,29 +50,59 @@ set_package_properties(
 
 set(_reason "")
 
-find_path(
-  Dbm_INCLUDE_DIR
-  NAMES dbm.h
-  PATH_SUFFIXES gdbm
-  DOC "Directory containing dbm library headers"
-)
-
-if(NOT Dbm_INCLUDE_DIR)
-  string(APPEND _reason "dbm.h not found. ")
+# If no compiler is loaded C library can't be checked anyway.
+if(NOT CMAKE_C_COMPILER_LOADED AND NOT CMAKE_CXX_COMPILER_LOADED)
+  set(Dbm_IS_BUILT_IN FALSE)
 endif()
 
-find_library(
-  Dbm_LIBRARY
-  NAMES
-    gdbm_compat
-    dbm
-    # TODO: Which system still has dbm built in the default C library? In such
-    # case this find module should be refactored to search the built-in library
-    # similar to how FindIconv does it. Otherwise, more likely, this should be
-    # removed from here.
-    c
-  DOC "The path to the dbm library"
-)
+if(NOT DEFINED Dbm_IS_BUILT_IN)
+  cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_QUIET TRUE)
+    check_symbol_exists(dbminit dbm.h Dbm_IS_BUILT_IN)
+  cmake_pop_check_state()
+endif()
+
+set(_Dbm_REQUIRED_VARS)
+if(Dbm_IS_BUILT_IN)
+  set(_Dbm_REQUIRED_VARS _Dbm_IS_BUILT_IN_MSG)
+  set(_Dbm_IS_BUILT_IN_MSG "built in to C library")
+else()
+  set(_Dbm_REQUIRED_VARS Dbm_INCLUDE_DIR Dbm_LIBRARY _Dbm_SANITY_CHECK)
+
+  find_path(
+    Dbm_INCLUDE_DIR
+    NAMES dbm.h
+    PATH_SUFFIXES gdbm
+    DOC "Directory containing dbm library headers"
+  )
+
+  if(NOT Dbm_INCLUDE_DIR)
+    string(APPEND _reason "dbm.h not found. ")
+  endif()
+
+  find_library(
+    Dbm_LIBRARY
+    NAMES
+      gdbm_compat
+      dbm
+    DOC "The path to the dbm library"
+  )
+
+  if(NOT Dbm_LIBRARY)
+    string(APPEND _reason "dbm library not found. ")
+  endif()
+
+  # Sanity check.
+  if(Dbm_LIBRARY)
+    check_library_exists("${Dbm_LIBRARY}" dbminit "" _Dbm_SANITY_CHECK)
+
+    if(NOT _dbm_sanity_check)
+      string(APPEND _reason "Sanity check failed: dbminit not found. ")
+    endif()
+  endif()
+
+  mark_as_advanced(Dbm_INCLUDE_DIR Dbm_LIBRARY)
+endif()
 
 if(Dbm_LIBRARY MATCHES "gdbm_compat")
   set(Dbm_IMPLEMENTATION "GDBM")
@@ -69,27 +110,10 @@ elseif(Dbm_LIBRARY)
   set(Dbm_IMPLEMENTATION "DBM")
 endif()
 
-if(NOT Dbm_LIBRARY)
-  string(APPEND _reason "dbm library not found. ")
-endif()
-
-# Sanity check.
-if(Dbm_LIBRARY)
-  check_library_exists("${Dbm_LIBRARY}" dbminit "" _dbm_sanity_check)
-
-  if(NOT _dbm_sanity_check)
-    string(APPEND _reason "Sanity check failed: dbminit not found. ")
-  endif()
-endif()
-
-mark_as_advanced(Dbm_INCLUDE_DIR Dbm_LIBRARY)
-
 find_package_handle_standard_args(
   Dbm
   REQUIRED_VARS
-    Dbm_LIBRARY
-    Dbm_INCLUDE_DIR
-    _dbm_sanity_check
+    ${_Dbm_REQUIRED_VARS}
   REASON_FAILURE_MESSAGE "${_reason}"
 )
 
@@ -99,16 +123,30 @@ if(NOT Dbm_FOUND)
   return()
 endif()
 
-set(Dbm_INCLUDE_DIRS ${Dbm_INCLUDE_DIR})
-set(Dbm_LIBRARIES ${Dbm_LIBRARY})
+if(Dbm_IS_BUILT_IN)
+  set(Dbm_INCLUDE_DIRS "")
+  set(Dbm_LIBRARIES "")
+else()
+  set(Dbm_INCLUDE_DIRS ${Dbm_INCLUDE_DIR})
+  set(Dbm_LIBRARIES ${Dbm_LIBRARY})
+endif()
 
 if(NOT TARGET Dbm::Dbm)
   add_library(Dbm::Dbm UNKNOWN IMPORTED)
 
-  set_target_properties(
-    Dbm::Dbm
-    PROPERTIES
-      IMPORTED_LOCATION "${Dbm_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${Dbm_INCLUDE_DIR}"
-  )
+  if(Dbm_INCLUDE_DIR)
+    set_target_properties(
+      Dbm::Dbm
+      PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${Dbm_INCLUDE_DIR}"
+    )
+  endif()
+
+  if(Dbm_LIBRARY)
+    set_target_properties(
+      Dbm::Dbm
+      PROPERTIES
+        IMPORTED_LOCATION "${Dbm_LIBRARY}"
+    )
+  endif()
 endif()
