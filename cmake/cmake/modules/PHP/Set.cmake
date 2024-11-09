424ca98775b2da0b8f409ba3fceb5c50e0656a5d
Peter Kokot <peterkokot@gmail.com>
2024-11-09 22:43:31 +0100

    Update PHP/Set help text

--- cmake/cmake/modules/PHP/Set.cmake
+++ cmake/cmake/modules/PHP/Set.cmake
@@ -1,58 +1,109 @@
 #[=============================================================================[
 Set a CACHE variable that depends on a set of conditions.
 
-At the time of writing, there are 3 main ways in CMake to create non-internal
-cache variables that can be also customized from the outside using the `-D`
-command-line option, through CMake presets, or similar:
+At the time of writing, there are three main ways in CMake to create
+non-internal cache variables that can also be customized externally via the `-D`
+command-line option, CMake presets, or similar:
+
 * `option()`
 * `set(<variable> <value> CACHE <type> <docstring>)`
 * `cmake_dependent_option()`
 
-Ideally, these are the recommended ways to set configuration variables. However,
-there are many cases where a `CACHE` variable of a type other than `BOOL`
-depends on certain conditions. Additionally, an edge-case issue with
+Ideally, these are the recommended methods to set configuration variables.
+However, there are cases where a `CACHE` variable of a type other than `BOOL`
+depends on specific conditions. Additionally, an edge-case issue with
 `cmake_dependent_option()` is that it sets a local variable if the conditions
-are not met. Local variables in such edge cases can be difficult to work with
-when using `add_subdirectory()`. In the parent scope, instead of the local
+are not met. Local variables in such cases can be difficult to work with
+when using `add_subdirectory()`. In the parent scope, instead of a local
 variable with a forced value, the cached variable is still defined as
 `INTERNAL`, which can lead to bugs in the build process.
 
-This module exposes the following function:
+## The `php_set()` function
 
 ```cmake
 php_set(
   <variable>
-  [TYPE <type>] | [[TYPE STRING] CHOICES <string>...]
-  [CHOICES_OPTIONAL]
-  [CHOICES_CASE_SENSITIVE]
+  TYPE <type>
   [IF <condition> VALUE <value> [ELSE_VALUE <default>]] | [VALUE <value>]
   DOC <docstring>...
 )
 ```
 
-It sets a CACHE `<variable>` of `<type>` to a `<value>`.
+This function sets a cache `<variable>` of `<type>` to a `<value>`.
 
 * `TYPE` can be `BOOL`, `FILEPATH`, `PATH`, or `STRING`.
 
-* `CHOICES` is an optional list of items when `STRING` type is used to create
-  a list of supported options to pick in the GUI. Under the hood, it sets the
-  `STRINGS` CACHE variable property.
+* `VALUE` is the default variable value. There are two ways to set the default
+  value.
 
-  When using `CHOICES`, the `VALUE` and `TYPE` keywords can be also omitted. The
-  default variable value is set to the first item in the `CHOICES` list and
-  default `TYPE` will be set to `STRING`.
+  * When using the `IF <condition>` argument, it sets the variable to `<value>`
+    if `<condition>` is met. Otherwise it sets the `<variable>` to `ELSE_VALUE`
+    `<default>` and hides it in the GUI if `ELSE_VALUE` is provided. Internally,
+    `ELSE_VALUE` will set an `INTERNAL` cache variable if `<condition>` is not
+    met. If `ELSE_VALUE` is not provided, the `INTERNAL` cache variable is not
+    set (it is undefined).
+
+    `IF` behaves the same as the `<depends>` argument in the
+    `cmake_dependent_option()`. This supports both full condition syntax and
+    semicolon-separated list of conditions.
+
+  * When using only `VALUE` signature, it sets the cache variable to `<value>`,
+    which is equivalent to writing:
+
+    ```cmake
+    set(<variable> <value> CACHE <type> <docstring>)
+    ```
+
+* `DOC` is a short help text for the variable, visible in GUIs. Multiple strings
+  are joined together.
+
+  For example:
+
+  ```cmake
+  php_set(
+    VAR
+    TYPE STRING
+    IF [[CMAKE_SYSTEM_NAME STREQUAL "Linux"]]
+    VALUE "some value"
+    DOC
+      "This help text "
+      "is joined "
+      "together."
+  )
+  ```
+
+## The `CHOICES` signature
+
+The `CHOICES` signature provides a list of options to choose from:
+
+```cmake
+php_set(
+  <variable>
+  [TYPE STRING]
+  CHOICES <string>...
+  [CHOICES_OPTIONAL]
+  [CHOICES_CASE_SENSITIVE]
+  [IF <condition> [VALUE <value>] [ELSE_VALUE <default>]] | [VALUE <value>]
+  DOC <docstring>...
+)
+```
+
+* `CHOICES` is a list of items to choose from in the GUI. Internally, it sets
+  the `STRINGS` cache variable property. The default `TYPE` is `STRING`, which
+  is optional.
+
+  When using `CHOICES`, the `VALUE` keyword is optional. The default variable
+  value is set to the first item in the `CHOICES` list.
 
   For example:
 
   ```cmake
   include(PHP/Set)
-
   php_set(
     VAR
     CHOICES auto on off
     DOC "Variable with default value set to the first list item"
   )
-
   message(STATUS "VAR=${VAR}")
   ```
 
@@ -62,69 +113,69 @@ It sets a CACHE `<variable>` of `<type>` to a `<value>`.
   VAR=auto
   ```
 
-  * When `CHOICES_OPTIONAL` is used, variable value will not be validated to
-    match one of the list items. By default, when using `CHOICES`, the variable
-    value must match one of the list items otherwise a fatal error is thrown.
+* When `CHOICES_OPTIONAL` is given, the variable value will not be required to
+  match one of the list items. By default, when using `CHOICES`, the variable
+  value must match one of the list items; otherwise, a fatal error is thrown.
 
-  * When `CHOICES_CASE_SENSITIVE` is used, variable value passed by the user
-    will need to be of the same case as defined in the `CHOICES` list. By
-    default, choices are case insensitive.
+  For example:
 
-    For example:
+  ```cmake
+  php_set(
+    VAR
+    CHOICES auto on off
+    CHOICES_OPTIONAL
+    DOC
+      "Variable with optional predefined choices where its value can be also "
+      "changed to anything else."
+  )
+  message(STATUS "VAR=${VAR}")
+  ```
 
-    ```cmake
-    php_set(
-      VAR
-      CHOICES auto unixODBC iODBC
-      DOC "Variable with a case-insensitive list of choices"
-    )
-    message(STATUS "VAR=${VAR}")
-    ```
+  ```sh
+  cmake -S <source-dir> -B <build-dir> -D VAR=overridden
+  ```
 
-    ```sh
-    cmake -S <source-dir> -B <build-dir> -D VAR=unixodbc
-    ```
+  Output:
 
-    Will output `VAR=unixODBC` and not `VAR=unixodbc`.
+  ```
+  VAR=overridden
+  ```
 
-    With `CHOICES_CASE_SENSITIVE`:
+* When `CHOICES_CASE_SENSITIVE` is given, the variable value will need to match
+  the case of item defined in the `CHOICES` list. By default, choices are
+  case-insensitive.
 
-    ```cmake
-    php_set(
-      VAR
-      CHOICES auto unixODBC iODBC
-      CHOICES_CASE_SENSITIVE
-      DOC "Variable with a case-sensitive list of choices"
-    )
-    message(STATUS "VAR=${VAR}")
-    ```
+  For example:
 
-    A fatal error will be thrown, if `VAR` is set to a case-sensitive value
-    `unixodbc`, which is not defined in the `CHOICES` list.
+  ```cmake
+  php_set(
+    VAR
+    CHOICES auto unixODBC iODBC
+    DOC "Variable with a case-insensitive list of choices"
+  )
+  message(STATUS "VAR=${VAR}")
+  ```
 
-* `VALUE` is the default variable value. There are two ways to set default
-  value.
+  ```sh
+  cmake -S <source-dir> -B <build-dir> -D VAR=unixodbc
+  ```
 
-  * When using the `IF <condition>` argument, it sets the variable to `<value>`
-    if `<condition>` is met. Otherwise it sets the `<variable>` to `ELSE_VALUE`
-    `<default>` and hides it in the GUI, if `ELSE_VALUE` is given. Under the
-    hood `ELSE_VALUE` will set `INTERNAL` cache variable if `<condition>` is not
-    met. If `ELSE_VALUE` is not provided, the `INTERNAL` cache variable is not
-    set (it is undefined).
+  Will output `VAR=unixODBC` and not `VAR=unixodbc`.
 
-    `IF` behaves the same as the `<depends>` argument in the
-    `cmake_dependent_option()`. This supports both full condition syntax and
-    semicolon-separated list of conditions.
+  With `CHOICES_CASE_SENSITIVE`:
 
-  * When using only `VALUE` signature, it sets the cache variable to `<value>`.
-    It is the same as writing:
+  ```cmake
+  php_set(
+    VAR
+    CHOICES auto unixODBC iODBC
+    CHOICES_CASE_SENSITIVE
+    DOC "Variable with a case-sensitive list of choices"
+  )
+  message(STATUS "VAR=${VAR}")
+  ```
 
-    ```cmake
-    set(<variable> <value> CACHE <type> <docstring>)
-    ```
-
-* `DOC` is a short variable help text visible in the GUIs. Multiple strings are
-  joined together.
+  A fatal error will be thrown, if `VAR` is set to a case-sensitive value
+  (`unixodbc`) that does not match any item in the `CHOICES` list.
 #]=============================================================================]
 
 include_guard(GLOBAL)
@@ -196,8 +247,8 @@ function(php_set)
   elseif(NOT DEFINED ${bufferVarName})
     # Initial configuration phase without variable set by the user.
 
-    # When using CHOICES and VALUE has not been set, set the variable value to
-    # the first CHOICES item.
+    # When using CHOICES and VALUE is not set, set the variable value to the
+    # first item of the CHOICES list.
     if(
       parsed_TYPE STREQUAL "STRING"
       AND DEFINED parsed_CHOICES
