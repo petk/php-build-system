47cbd66ad66cd47de4b026ce109d0675e954191c
Peter Kokot <peterkokot@gmail.com>
2024-11-13 14:19:55 +0100

    Refactor FPM checks (#8)
    - Cross-compilation enhancements to be able to set PROC_MEM_FILE
    - mach_vm_read check simplified as the signature is a bit different
      vm_map_read_t vs vm_map_t first argument. The `check_symbol_exists()`
      is sufficient check in this case and avoids writing entire signature.
    - clock_get*time() check moved to fpm's CMakeLists.txt directly
    - Some FPM checks now have a bit simpler configure log/output messages
    - The toolchains template improved and refactored
    - Fixed some nits in FindNetSnmp module

--- cmake/cmake/toolchains/template.cmake
+++ cmake/cmake/toolchains/template.cmake
@@ -8,8 +8,19 @@ set(CMAKE_SYSTEM_PROCESSOR "")
 set(CMAKE_FIND_ROOT_PATH "")
 
 # This is a list of all exit codes or result variables required when
-# cross-compiling to help identify the target system when cross-compiling
-# emulator (CMAKE_CROSSCOMPILING_EMULATOR) is not available.
+# cross-compiling to help identify the target system when the
+# CMAKE_CROSSCOMPILING_EMULATOR is not available or other adjustments are needed
+# for the target system.
+
+################################################################################
+# PHP
+################################################################################
+
+# Set the exit code for the fopencookie seeker using off64_t check.
+set(COOKIE_SEEKER_USES_OFF64_T_EXITCODE 0)
+
+# Set the exit code for the getaddrinfo() check.
+set(HAVE_GETADDRINFO_EXITCODE 0)
 
 # Set the exit codes for the alignment segments checks.
 # See PHP/CheckSegmentsAlignment.cmake
@@ -22,66 +33,80 @@ set(PHP_HAVE_ZMAX_PAGE_SIZE_CXX_EXITCODE 0)
 # (relevant only for GCC version 8.2).
 set(PHP_HAVE_BROKEN_OPTIMIZE_STRLEN_EXITCODE 1)
 
-# Set the exit code for the clock_get_time() check.
-set(HAVE_CLOCK_GET_TIME_EXITCODE 0)
+# Set the exit code to 1 when using Clang 17 or later and -fno-sanitize=function
+# needs to be added for the PHP_UNDEFINED_SANITIZER option, otherwise set to 0.
+set(PHP_HAVE_UBSAN_EXITCODE 0)
 
-# Set the exit code if flush should be called explicitly after a buffered io.
-set(HAVE_FLUSHIO_EXITCODE 1)
-
-# Set the exit code for the POSIX fnmatch() check.
-set(HAVE_FNMATCH_EXITCODE 0)
-
-# Set the exit code for the check whether the curl library is linked with
-# OpenSSL 1.1 or earlier.
-set(HAVE_CURL_OLD_OPENSSL_EXITCODE 4)
-
-# Set the exit code for the check whether the bundled PCRE library has JIT
-# supported on the target architecture.
-set(HAVE_PCRE_JIT_SUPPORT_EXITCODE 0)
-
-# Set the exit code for the fopencookie seeker using off64_t check.
-set(COOKIE_SEEKER_USES_OFF64_T_EXITCODE 0)
-
-# Set the exit code for the check of reentrant time-related functions being HP-UX
-# style.
+# Set the exit code for the check of reentrant time-related functions being
+# HP-UX style.
 set(PHP_HPUX_TIME_R_EXITCODE 1)
 
 # Set the exit code for the check of reentrant time-related functions being IRIX
 # style.
 set(PHP_IRIX_TIME_R_EXITCODE 1)
 
-# Set the exit code for the check whether the external gd library has support
-# for the given format.
-set(HAVE_GD_PNG_EXITCODE 1)
-set(HAVE_GD_AVIF_EXITCODE 1)
-set(HAVE_GD_WEBP_EXITCODE 1)
-set(HAVE_GD_JPG_EXITCODE 1)
-set(HAVE_GD_XPM_EXITCODE 1)
-set(HAVE_GD_BMP_EXITCODE 1)
-set(HAVE_GD_TGA_EXITCODE 1)
+# Set the exit code for the writing to stdout check.
+set(PHP_WRITE_STDOUT_EXITCODE 0)
+
+# Set the exit code of the byte ordering fallback check. When targeting a
+# big-endian system, set it to 0, and for little-endian set it to 1.
+set(WORDS_BIGENDIAN_EXITCODE 1)
+
+################################################################################
+# Zend Engine
+################################################################################
 
 # Set the exit code for the check if syscall to create shadow stack exists.
 set(SHADOW_STACK_SYSCALL_EXITCODE 1)
 
-# Set the exit code for the getaddrinfo() check.
-set(HAVE_GETADDRINFO_EXITCODE 0)
-
-# Set the exit code for the writing to stdout check.
-set(PHP_WRITE_STDOUT_EXITCODE 0)
-
 # Set the exit code of the stack limit check.
 set(ZEND_CHECK_STACK_LIMIT_EXITCODE 1)
 
-# Set the exit code of the ttyname_r check.
-set(HAVE_TTYNAME_R_EXITCODE 0)
+# Set the exit code and the output of the ZEND_MM check.
+# See CheckMMAlignment.cmake.
+set(ZEND_MM_EXITCODE 0)
+set(ZEND_MM_EXITCODE__TRYRUN_OUTPUT "(size_t)8 (size_t)3 0")
 
-# Set the exit code of the pread check.
-set(HAVE_PREAD_EXITCODE 0)
-set(PHP_PREAD_64_EXITCODE 0)
+################################################################################
+# sapi/fpm
+################################################################################
 
-# Set the exit code of the pwrite check.
-set(HAVE_PWRITE_EXITCODE 0)
-set(PHP_PWRITE_64_EXITCODE 0)
+# Set the exit code for the clock_get_time() check.
+set(HAVE_CLOCK_GET_TIME_EXITCODE 0)
+
+# Set the exit code of the ptrace check for PHP FPM.
+set(HAVE_PTRACE_EXITCODE 0)
+
+# Set the process memory access file - 'mem' on Linux-alike or 'as' on
+# Solaris-alike target systems for the PHP FPM to use pread trace type.
+# (/proc/<pid>/<mem-or-as>)
+set(PROC_MEM_FILE mem)
+
+################################################################################
+# ext/curl
+################################################################################
+
+# Set the exit code for the check whether the curl library is linked with
+# OpenSSL 1.1 or earlier.
+set(HAVE_CURL_OLD_OPENSSL_EXITCODE 4)
+
+################################################################################
+# ext/gd
+################################################################################
+
+# Set the exit code for the check whether the external gd library has support
+# for the given format.
+set(HAVE_GD_AVIF_EXITCODE 1)
+set(HAVE_GD_BMP_EXITCODE 1)
+set(HAVE_GD_JPG_EXITCODE 1)
+set(HAVE_GD_PNG_EXITCODE 1)
+set(HAVE_GD_TGA_EXITCODE 1)
+set(HAVE_GD_WEBP_EXITCODE 1)
+set(HAVE_GD_XPM_EXITCODE 1)
+
+################################################################################
+# ext/iconv
+################################################################################
 
 # Set the exit code of the iconv //IGNORE check.
 set(ICONV_BROKEN_IGNORE_EXITCODE 1)
@@ -89,8 +114,9 @@ set(ICONV_BROKEN_IGNORE_EXITCODE 1)
 # Set the exit code of the iconv errno check.
 set(PHP_ICONV_ERRNO_WORKS_EXITCODE 0)
 
-# Set the exit code of the ptrace check for PHP FPM.
-set(HAVE_PTRACE_EXITCODE 0)
+################################################################################
+# ext/opcache
+################################################################################
 
 # Set the exit code of SysV IPC shared memory check in opcache extension.
 set(HAVE_SHM_IPC_EXITCODE 0)
@@ -102,23 +128,47 @@ set(HAVE_SHM_MMAP_ANON_EXITCODE 0)
 # Set the exit code of the shm_open() shared memory check in opcache extension.
 set(HAVE_SHM_MMAP_POSIX_EXITCODE 0)
 
-# Set the exit code to 1 when using Clang 17 or later and -fno-sanitize=function
-# needs to be added for the PHP_UNDEFINED_SANITIZER option, otherwise set to 0.
-set(PHP_HAVE_UBSAN_EXITCODE 0)
+################################################################################
+# ext/pcre
+################################################################################
 
-# Set the exit code of the byte ordering fallback check. When targeting a
-# big-endian system, set it to 0, and for little-endian set it to 1.
-set(WORDS_BIGENDIAN_EXITCODE 1)
+# Set the exit code for the check whether the bundled PCRE library has JIT
+# supported on the target architecture.
+set(HAVE_PCRE_JIT_SUPPORT_EXITCODE 0)
 
-# Set exit codes for the algos checks when using external crypt library
+################################################################################
+# ext/posix
+################################################################################
+
+# Set the exit code of the ttyname_r check.
+set(HAVE_TTYNAME_R_EXITCODE 0)
+
+################################################################################
+# ext/session
+################################################################################
+
+# Set the exit code of the pread check.
+set(HAVE_PREAD_EXITCODE 0)
+set(PHP_PREAD_64_EXITCODE 0)
+
+# Set the exit code of the pwrite check.
+set(HAVE_PWRITE_EXITCODE 0)
+set(PHP_PWRITE_64_EXITCODE 0)
+
+################################################################################
+# ext/standard
+################################################################################
+
+# Set the exit code if flush should be called explicitly after a buffered io.
+set(HAVE_FLUSHIO_EXITCODE 1)
+
+# Set the exit code for the POSIX fnmatch() check.
+set(HAVE_FNMATCH_EXITCODE 0)
+
+# Set the exit codes for the algos checks when using external crypt library
 # (EXT_STANDARD_EXTERNAL_LIBCRYPT).
+set(_PHP_CRYPT_HAVE_BLOWFISH_EXITCODE 0)
 set(_PHP_CRYPT_HAVE_EXT_DES_EXITCODE 0)
 set(_PHP_CRYPT_HAVE_MD5_EXITCODE 0)
-set(_PHP_CRYPT_HAVE_BLOWFISH_EXITCODE 0)
 set(_PHP_CRYPT_HAVE_SHA256_EXITCODE 0)
 set(_PHP_CRYPT_HAVE_SHA512_EXITCODE 0)
-
-# Set the exit code and the output of the ZEND_MM check.
-# See CheckMMAlignment.cmake.
-set(ZEND_MM_EXITCODE 0)
-set(ZEND_MM_EXITCODE__TRYRUN_OUTPUT "(size_t)8 (size_t)3 0")
