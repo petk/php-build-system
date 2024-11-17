126ffbe2e9b6d9f9ce56d5b14ef776495e4726fb
Peter Kokot <peterkokot@gmail.com>
2024-11-17 21:09:21 +0100

    Update bin/make-docs.php script
    - Script now processes also ext, sapi, and pear subdirectories
    - Module generated docs include some basic generic usage for easier
      understanding

--- bin/make-docs.php
+++ bin/make-docs.php
@@ -11,79 +11,14 @@ declare(strict_types=1);
  */
 
 /**
- * Generate CMake module Markdown docs file from the file's header comment.
- */
-function generateModuleDocs(
-    string $file,
-    string $namespace,
-    string $destination,
-    string $url,
-): void {
-    $content = file_get_contents($file);
-    preg_match('/^#\[===+\[\s*(.*?)\s*#\]===+\]/s', $content, $matches);
-
-    if (!isset($matches[1])) {
-        return;
-    }
-
-    $header = $matches[1];
-
-    $moduleName = basename($file, '.cmake');
-
-    $content = '';
-    $content .= "# $namespace" . "$moduleName\n\n";
-    $content .= "See: [$moduleName.cmake]($url)\n\n";
-    if ($namespace) {
-        $content .= <<<EOT
-            ## Basic usage
-
-            ```cmake
-            include({$namespace}{$moduleName})
-            ```
-            EOT;
-    } elseif (1 === preg_match('/^Find(.+)$/', $moduleName, $matches)) {
-        if (isset($matches[1])) {
-            $package = $matches[1];
-        }
-        $content .= <<<EOT
-            ## Basic usage
-
-            ```cmake
-            find_package($package)
-            ```
-            EOT;
-    } else {
-        $content .= <<<EOT
-            ## Basic usage
-
-            ```cmake
-            include(cmake/$moduleName.cmake)
-            ```
-            EOT;
-    }
-    $content .= "\n\n";
-    $content .= $header;
-    $content .= "\n";
-
-    if (!file_exists($destination)) {
-        mkdir($destination, 0777, true);
-    }
-
-    $markdownFile = $destination . '/' . $moduleName . '.md';
-    if (file_exists($markdownFile)) {
-        echo "\nWarning: $markdownFile already exists.\n";
-        $markdownFile = $destination . '/' . $moduleName . '_2.md';
-        echo "Module has been renamed to $markdownFile\n\n";
-    }
-
-    file_put_contents($markdownFile, $content);
-}
-
-/**
  * Remove directory contents recursively.
  */
 function emptyDirectory(string $path): void
 {
+    if (!file_exists($path)) {
+        return;
+    }
+
     $iterator = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
         RecursiveIteratorIterator::CHILD_FIRST,
@@ -98,13 +33,142 @@ function emptyDirectory(string $path): void
     }
 }
 
-$modulesDocsDir = __DIR__ . '/../docs/cmake/modules';
+/**
+ * Generate CMake module Markdown docs file from the file's header comment.
+ */
+function generateModuleDocs(
+    string $file,
+    string $namespace,
+    string $destination,
+    string $url,
+): void {
+    if (!file_exists($file)) {
+        return;
+    }
 
-if (!file_exists($modulesDocsDir)) {
-    mkdir($modulesDocsDir, 0777, true);
-} else {
-    emptyDirectory($modulesDocsDir);
+    $code = file_get_contents($file);
+    preg_match('/^#\[===+\[\s*(.*?)\s*#\]===+\]/s', $code, $matches);
+
+    if (!isset($matches[1])) {
+        return;
+    }
+
+    $moduleName = basename($file, '.cmake');
+    $content = $matches[1];
+
+    $markdown = "<!-- This is auto-generated file. -->\n";
+    $markdown .= "# $namespace" . "$moduleName\n\n";
+    $markdown .= "* Module source code: [$moduleName.cmake]($url)\n\n";
+    $markdown .= $content . "\n";
+
+    if ($namespace) {
+        $footer = <<<EOT
+            ## Basic usage
+
+            ```cmake
+            # CMakeLists.txt
+            include({$namespace}{$moduleName})
+            ```
+            EOT;
+    } elseif (1 === preg_match('/^Find(.+)$/', $moduleName, $matches)) {
+        $findPackageName = $matches[1] ?? null;
+        $findPackageUpper = strtoupper($findPackageName);
+
+        $footer = <<<EOT
+            ## Basic usage
+
+            ```cmake
+            # CMakeLists.txt
+            find_package($findPackageName)
+            ```
+
+            ## Customizing search locations
+
+            To customize where to look for the $findPackageName package base
+            installation directory, a common `CMAKE_PREFIX_PATH` or
+            package-specific `{$findPackageUpper}_ROOT` variable can be set at
+            the configuration phase. For example:
+
+            ```sh
+            cmake -S <source-dir> -B <build-dir> -DCMAKE_PREFIX_PATH="/opt/$findPackageName;/opt/some-other-package"
+            # or
+            cmake -S <source-dir> \
+                -B <build-dir> \
+                -D{$findPackageUpper}_ROOT=/opt/$findPackageName \
+                -DSOMEOTHERPACKAGE_ROOT=/opt/some-other-package
+            ```
+            EOT;
+    } else {
+        $footer = <<<EOT
+            ## Basic usage
+
+            ```cmake
+            # CMakeLists.txt
+            include(cmake/$moduleName.cmake)
+            ```
+            EOT;
+    }
+
+    if (1 !== preg_match('/## Basic usage[\r\n]/', $content, $matches)) {
+        $markdown .= "\n" . $footer . "\n";
+    }
+
+    if (!file_exists($destination)) {
+        mkdir($destination, 0777, true);
+    }
+
+    $markdownFile = realpath($destination) . '/' . $moduleName . '.md';
+    if (file_exists($markdownFile)) {
+        echo "\nWarning: $markdownFile already exists.\n";
+        $markdownFile = $destination . '/' . $moduleName . '_2.md';
+        echo "Documentation file has been renamed to $markdownFile\n\n";
+    }
+
+    echo "Generating $markdownFile\n";
+    file_put_contents($markdownFile, $markdown);
 }
+
+/**
+ * Generate Markdown documentation from the extension or SAPI CMakeLists.txt
+ * header comment.
+ */
+function generateDocs(
+    string $file,
+    string $destination,
+): void {
+    if (!file_exists($file)) {
+        return;
+    }
+
+    $code = file_get_contents($file);
+    preg_match('/^#\[===+\[\s*(.*?)\s*#\]===+\]/s', $code, $matches);
+
+    if (!isset($matches[1])) {
+        return;
+    }
+
+    $content = $matches[1];
+
+    $relativeFilename = trim(str_replace(realpath(__DIR__ . '/../cmake'), '', realpath($file)), '/');
+    $url = 'https://github.com/petk/php-build-system/blob/master/cmake/' . $relativeFilename;
+
+    $markdown = "<!-- This is auto-generated file. -->\n";
+    $markdown .= "* Source code: [$relativeFilename]($url)\n\n";
+    $markdown .= $content . "\n";
+
+    if (!file_exists(dirname($destination))) {
+        mkdir(dirname($destination), 0777, true);
+    }
+
+    echo 'Generating ' . $destination . "\n";
+    file_put_contents($destination, $markdown);
+}
+
+/*
+ * Generate documentation for CMake modules.
+ */
+
+emptyDirectory(__DIR__ . '/../docs/cmake/modules');
 
 $modules = [
     'cmake/modules',
@@ -148,11 +212,32 @@ foreach ($files as $module) {
         }
     }
 
-    echo "Processing cmake/$relativeFilename\n";
     generateModuleDocs(
         $module,
         $namespace,
-        $modulesDocsDir . '/' . $subdir,
+        __DIR__ . '/../docs/cmake/modules/' . $subdir,
         'https://github.com/petk/php-build-system/blob/master/cmake/' . $relativeFilename,
     );
 }
+
+/*
+ * Generate documentation for PHP SAPIs and extensions.
+ */
+
+emptyDirectory(__DIR__ . '/../docs/cmake/ext');
+emptyDirectory(__DIR__ . '/../docs/cmake/sapi');
+
+$files = glob(__DIR__ . '/../cmake/{ext,sapi}/*/CMakeLists.txt', GLOB_BRACE);
+
+foreach ($files as $file) {
+    $destination = __DIR__
+        . '/../docs/cmake/'
+        . basename(dirname($file, 2))
+        . '/'
+        . basename(dirname($file))
+        . '.md';
+    generateDocs($file, $destination);
+}
+
+// Generate PEAR docs.
+generateDocs(__DIR__ . '/../cmake/pear/CMakeLists.txt', __DIR__ . '/../docs/cmake/pear.md');
