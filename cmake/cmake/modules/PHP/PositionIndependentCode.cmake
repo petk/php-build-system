7e26b858d0de7cb429b4d10b17ee7076f67bbc25
Peter Kokot <peterkokot@gmail.com>
2025-03-09 20:19:47 +0100

    Update PositionIndependentCode module
    - Logging refactored a bit further
    - Docs updated a bit

--- cmake/cmake/modules/PHP/PositionIndependentCode.cmake
+++ cmake/cmake/modules/PHP/PositionIndependentCode.cmake
@@ -1,15 +1,18 @@
 #[=============================================================================[
 # PHP/PositionIndependentCode
 
-Check whether to enable the `POSITION_INDEPENDENT_CODE` or not for all targets.
-The SHARED and MODULE targets have PIC enabled regardless of this option.
+Wrapper module for CMake's `CheckPIESupported` module and
+`CMAKE_POSITION_INDEPENDENT_CODE` variable.
 
-TODO: This unconditionally enables position independent code globally, to be
-able to build shared apache2handler, embed, and phpdbg SAPIs. Probably could be
-fine tuned in the future better but it can exponentially complicate the build
-system code or the build usability.
+This module checks whether to enable the `POSITION_INDEPENDENT_CODE` target
+property for all targets globally. The SHARED and MODULE targets have PIC always
+enabled by default regardless of this module.
 
-https://cmake.org/cmake/help/latest/variable/CMAKE_POSITION_INDEPENDENT_CODE.html
+Position independent code (PIC) and position independent executable (PIE)
+compile-time and link-time options are for now unconditionally added globally to
+all targets, to be able to build shared apache2handler, embed, and phpdbg SAPI
+libraries. This probably could be fine tuned in the future further but it can
+exponentially complicate the build system code or the build usability.
 
 ## Usage
 
@@ -23,14 +26,34 @@ include_guard(GLOBAL)
 
 block()
   include(CheckPIESupported)
+
+  message(CHECK_START "Checking if linker supports PIE")
+
   check_pie_supported(OUTPUT_VARIABLE output)
-  if(NOT CMAKE_C_LINK_PIE_SUPPORTED)
-    message(
-      WARNING
-      "Position independent executable (PIE) is not supported at link time: "
-      "${output}.\n"
-      "PIE link options will not be passed to linker."
+
+  if(CMAKE_C_LINK_PIE_SUPPORTED)
+    message(CHECK_PASS "yes")
+  else()
+    message(CHECK_FAIL "no")
+
+    if(
+      CMAKE_VERSION VERSION_GREATER_EQUAL 3.26
+      AND NOT DEFINED _PHP_POSITION_INDEPENDENT_CODE_LOGGED
     )
+      message(
+        CONFIGURE_LOG
+        "Position independent executable (PIE) is not supported at link time:\n"
+        "${output}"
+        "PIE link options will not be passed to linker."
+      )
+
+      set(
+        _PHP_POSITION_INDEPENDENT_CODE_LOGGED
+        TRUE
+        CACHE INTERNAL
+        "Internal marker whether PIE check has been logged."
+      )
+    endif()
   endif()
 endblock()
 
