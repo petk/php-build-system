d7210f2ce0a9192e526850c435f48ed6e0655fd7
Peter Kokot <peterkokot@gmail.com>
2025-06-12 23:44:44 +0200

    Fix build with bzip2 version 1.0.1
    This is for consistency, because sanity check fails because FILE is not
    defined in bzip2 1.0.1 and earlier.

--- cmake/cmake/Configuration.cmake
+++ cmake/cmake/Configuration.cmake
@@ -206,9 +206,6 @@ set(PHP_POSTGRESQL_MIN_VERSION 9.1)
 # Minimum required version for the zlib dependency.
 set(PHP_ZLIB_MIN_VERSION 1.2.0.4)
 
-# Minimum required version for the BZip2 dependency.
-set(PHP_BZIP2_MIN_VERSION 1.0.0)
-
 # Additional metadata for external packages to avoid duplication.
 set_package_properties(
   BZip2
