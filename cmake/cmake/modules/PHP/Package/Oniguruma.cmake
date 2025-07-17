934ced24d8f52380f35cc0b100a531bcae846642
Peter Kokot <peterkokot@gmail.com>
2025-07-17 17:06:06 +0200

    Enable PIC in Oniguruma

--- cmake/cmake/modules/PHP/Package/Oniguruma.cmake
+++ cmake/cmake/modules/PHP/Package/Oniguruma.cmake
@@ -66,6 +66,7 @@ macro(_php_package_oniguruma_download)
       -DINSTALL_DOCUMENTATION=OFF
       -DBUILD_TEST=OFF
       -DBUILD_SHARED_LIBS=OFF
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON
   )
 
   ExternalProject_Add(
