7ab0705c8ea427006df56e30a1f052a687fe800e
Peter Kokot <peterkokot@gmail.com>
2025-08-31 18:12:12 +0200

    Refactor Zend configuration options
    - All Zend related configuration moved to CMakeLists.txt for easier
      overview.
    - All configuration variables prefixed with 'PHP_'. Zend Engine is part
      of PHP anyway and doesn't seem to be intended to become a standalone
      project anymore neither anytime soon.

--- cmake/Zend/cmake/MaxExecutionTimers.cmake
+++ cmake/Zend/cmake/MaxExecutionTimers.cmake
@@ -1,44 +1,33 @@
 #[=============================================================================[
 Check whether to enable Zend max execution timers.
 
-Cache variables:
-
-* ZEND_MAX_EXECUTION_TIMERS
-
 Result variables:
 
 * ZEND_MAX_EXECUTION_TIMERS
 
-  A local variable based on the cache variable and thread safety to be able to
-  run consecutive configuration phases. When ZEND_MAX_EXECUTION_TIMERS cache
-  variable is set to 'auto', local variable default value is set to the
-  value of PHP_THREAD_SAFETY variable.
+  A local variable based on the configuration variable and thread safety to be
+  able to run consecutive configuration phases. When
+  PHP_ZEND_MAX_EXECUTION_TIMERS variable is set to 'auto',
+  ZEND_MAX_EXECUTION_TIMERS default value is set to the value of
+  PHP_THREAD_SAFETY variable.
 #]=============================================================================]
-
-message(CHECK_START "Checking whether to enable Zend max execution timers")
-
-if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-  message(CHECK_FAIL "no")
-  return()
-endif()
 
 include(FeatureSummary)
 include(PHP/SearchLibraries)
 
-set(
-  ZEND_MAX_EXECUTION_TIMERS
-  "auto"
-  CACHE STRING
-  "Enable Zend max execution timers"
-)
-mark_as_advanced(ZEND_MAX_EXECUTION_TIMERS)
-set_property(CACHE ZEND_MAX_EXECUTION_TIMERS PROPERTY STRINGS "auto" "ON" "OFF")
+set(ZEND_MAX_EXECUTION_TIMERS FALSE)
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+  return()
+endif()
+
+message(CHECK_START "Checking whether to enable Zend max execution timers")
 
 # Set a local variable based on the cache variable.
-if(ZEND_MAX_EXECUTION_TIMERS STREQUAL "auto")
+if(PHP_ZEND_MAX_EXECUTION_TIMERS STREQUAL "auto")
   set(ZEND_MAX_EXECUTION_TIMERS "${PHP_THREAD_SAFETY}")
 else()
-  set(ZEND_MAX_EXECUTION_TIMERS "${ZEND_MAX_EXECUTION_TIMERS}")
+  set(ZEND_MAX_EXECUTION_TIMERS "${PHP_ZEND_MAX_EXECUTION_TIMERS}")
 endif()
 
 if(ZEND_MAX_EXECUTION_TIMERS AND CMAKE_SYSTEM_NAME STREQUAL "Linux")
@@ -52,10 +41,10 @@ if(ZEND_MAX_EXECUTION_TIMERS AND CMAKE_SYSTEM_NAME STREQUAL "Linux")
   )
 
   if(NOT PHP_ZEND_HAS_TIMER_CREATE)
-    set(ZEND_MAX_EXECUTION_TIMERS OFF)
+    set(ZEND_MAX_EXECUTION_TIMERS FALSE)
   endif()
 else()
-  set(ZEND_MAX_EXECUTION_TIMERS OFF)
+  set(ZEND_MAX_EXECUTION_TIMERS FALSE)
 endif()
 
 if(ZEND_MAX_EXECUTION_TIMERS)
