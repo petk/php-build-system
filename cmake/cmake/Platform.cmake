a64e4fd5d8ad2adde02d42123d86040063cfd44d
Peter Kokot <peterkokot@gmail.com>
2025-09-11 20:07:26 +0200

    Refactor internal CMake modules
    Modules located in the cmake/modules/PHP directory should ideally be
    useful not only in php-src but also in PHP extensions scope. This
    separates internal modules that are only used when building php-src.
    
    Additionally, investigated the deprecated/obsolete prehistoric
    _REENTRANT and _THREAD_SAFE compile definitions further and added notes
    in the comments.

--- cmake/cmake/Platform.cmake
+++ cmake/cmake/Platform.cmake
@@ -4,23 +4,6 @@ Platform-specific configuration.
 
 include_guard(GLOBAL)
 
-# Check unused linked libraries on executable and shared/module library targets.
-include(PHP/LinkWhatYouUse)
-
-# Enable C and POSIX extensions.
-include(PHP/SystemExtensions)
-
-# The above system extensions will be defined in the main/php_config.h. The
-# php-src code at the time of writing doesn't include C headers in order to
-# utilize them. The main/php_config.h should be the first inclusion before
-# including any system header. Perhaps in php.h file. Until then, the compile
-# definitions also need to be added when compiling and using PHP API. Mainly the
-# _GNU_SOURCE.
-target_link_libraries(php_config INTERFACE PHP::SystemExtensions)
-
-# Define GNU standard installation directories.
-include(GNUInstallDirs)
-
 # Platform-specific configuration. When cross-compiling, the host and target can
 # be different values with different configurations.
 if(NOT CMAKE_HOST_SYSTEM_NAME EQUAL CMAKE_SYSTEM_NAME)
