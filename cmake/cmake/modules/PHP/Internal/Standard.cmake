a5e8a822256fba3859c7c9fae9764390414b9d3a
Peter Kokot <peterkokot@gmail.com>
2026-02-21 00:25:24 +0100

    Fix nits

--- cmake/cmake/modules/PHP/Internal/Standard.cmake
+++ cmake/cmake/modules/PHP/Internal/Standard.cmake
@@ -7,11 +7,12 @@ Load this module in a CMake project with:
   include(PHP/Internal/Standard)
 
 This module sets the minimum required C standard for building and using PHP. It
-checks if current C compiler supports the specified C standard and emits an
-error if it doesn't. CMake by default already emits error in both cases, when
-using CMAKE_C_STANDARD variable with CMAKE_C_STANDARD_REQUIRED or the c_std_XX
-compile feature. This module provides a more informational error message for
-user to be aware of using a more recent and compatible compiler.
+verifies that the current C compiler supports the specified C standard and emits
+an error if it does not. CMake already emits error, when using the
+CMAKE_C_STANDARD variable together with CMAKE_C_STANDARD_REQUIRED, or when
+specifying the c_std_XX compile feature. However, this module provides a more
+informative and user-friendly error message, clearly indicating that a more
+recent and compatible compiler is required.
 #]=============================================================================]
 
 include_guard(GLOBAL)
@@ -23,10 +24,21 @@ block(PROPAGATE CMAKE_C_STANDARD)
   set(unsupported_standards 90)
 
   if(CMAKE_C_STANDARD_LATEST IN_LIST unsupported_standards)
+    set(compiler "C compiler \"${CMAKE_C_COMPILER_ID}\"")
+
+    if(CMAKE_C_COMPILER_VERSION)
+      string(APPEND compiler " version ${CMAKE_C_COMPILER_VERSION}")
+    endif()
+
+    string(APPEND compiler " (${CMAKE_C_COMPILER})")
+
     message(
       FATAL_ERROR
-      "PHP source code requires C${standard} standard or newer. Current C "
-      "compiler ${CMAKE_C_COMPILER} supports only C${CMAKE_C_STANDARD_LATEST}."
+      "PHP requires the language dialect \"C${standard}\". The current "
+      "${compiler} does not support this, or CMake version ${CMAKE_VERSION} "
+      "does not know the flags to enable it (the latest supported and known "
+      "standard is \"C${CMAKE_C_STANDARD_LATEST}\"). Please, update the "
+      "compiler."
     )
   endif()
 
@@ -38,8 +50,8 @@ block(PROPAGATE CMAKE_C_STANDARD)
   if(CMAKE_C_STANDARD IN_LIST unsupported_standards)
     message(
       WARNING
-      "PHP source code requires C${standard} standard or newer. "
-      "CMAKE_C_STANDARD has been set to '${standard}'."
+      "PHP requires \"C${standard}\" standard or newer. CMAKE_C_STANDARD has "
+      "been set to '${standard}'."
     )
     set(CMAKE_C_STANDARD ${standard})
   endif()
