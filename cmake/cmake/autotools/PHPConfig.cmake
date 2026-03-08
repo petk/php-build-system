d20ba4963bf238a51fc721f8f2a1d95101c48c43
Peter Kokot <peterkokot@gmail.com>
2026-03-08 16:40:21 +0100

    Provide CMake-related installation files to Autotools
    This installs CMake package config file and PHP CMake modules for
    building PHP extensions with CMake when PHP is built and installed on
    the host system with Autotools.

---
+++ cmake/cmake/autotools/PHPConfig.cmake
@@ -0,0 +1,62 @@
+#[=============================================================================[
+PHP package configuration file
+
+Finds PHP, the general purpose scripting language:
+
+  find_package(PHP [<version>] [COMPONENTS <components>...])
+
+This file is part of the PHP Autotools build system. See FindPHP module for more
+info.
+#]=============================================================================]
+
+################################################################################
+# Create a user-friendly not-found message for unsupported CMake versions.
+################################################################################
+
+if(CMAKE_VERSION VERSION_LESS 4.2)
+  string(
+    CONCAT
+    ${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE
+    "${CMAKE_FIND_PACKAGE_NAME} ${${CMAKE_FIND_PACKAGE_NAME}_VERSION} requires "
+    "CMake 4.2 or higher.\n"
+    "You are running CMake version ${CMAKE_VERSION}"
+  )
+
+  set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+
+  return()
+endif()
+
+cmake_minimum_required(VERSION 4.2...4.3)
+
+# No components given, look for default components.
+if(NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS)
+  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS Interpreter Development)
+endif()
+
+################################################################################
+# Use the find module to help find PHP.
+################################################################################
+
+include(${CMAKE_CURRENT_LIST_DIR}/FindPHP.cmake)
+
+################################################################################
+# Set result variables.
+################################################################################
+
+# Set path where additional PHP CMake modules are installed:
+set(PHP_CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
+list(APPEND CMAKE_MODULE_PATH ${PHP_CMAKE_MODULE_PATH})
+
+################################################################################
+# Include internal common modules when working with PHP.
+################################################################################
+
+if("Development" IN_LIST ${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS)
+  include(PHP/Internal/Configuration)
+
+  if(TARGET PHP::Extension)
+    include(PHP/SystemExtensions)
+    target_link_libraries(PHP::Extension INTERFACE PHP::SystemExtensions)
+  endif()
+endif()
