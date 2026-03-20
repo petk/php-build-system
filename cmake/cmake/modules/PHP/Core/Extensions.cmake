d828ad2c79215b65532560db84d126266f15e204
Peter Kokot <peterkokot@gmail.com>
2026-03-20 19:30:30 +0100

    Fix extension dependencies
    This simplifies checking of the extension dependencies by using custom
    target properties:
    
    * PHP_REQUIRED_EXTENSIONS
    * PHP_OPTIONAL_EXTENSIONS
    * PHP_RECOMMENDED_EXTENSIONS
    * PHP_CONFLICTING_EXTENSIONS
    
    With this, CMakeLists files aren't parsed in advance anymore and
    extensions aren't automatically enabled/reconfigured anymore. It is too
    inconvenient to make the PHP extensions configuration automatic in the
    CMake configuration phase. Instead the summary at the end of the
    configure phase provides info about extension dependencies and
    configuration issues for user to manually intervene and reconfigure.
    
    Additionally:
    - Global property PHP_EXTENSIONS renamed to PHP_ENABLED_EXTENSIONS for
      clarity.
    - Global property PHP_SAPIS renamed to PHP_ENABLED_SAPIS for clarity.
    - A list of always enabled extensions removed.
    
    Fixes #49

--- cmake/cmake/modules/PHP/Core/Extensions.cmake
+++ cmake/cmake/modules/PHP/Core/Extensions.cmake
@@ -1,34 +1,29 @@
 #[=============================================================================[
 This is an internal module and is intended for usage only within the php-src.
-It provides commands for parsing CMakeLists.txt files of PHP extensions and
-sorting extensions based on the dependencies listed in the add_dependencies().
+It provides commands for sorting PHP extensions based on their dependencies.
 
 Load this module in a CMake project with:
 
   include(PHP/Core/Extensions)
 
-If an extension has specified dependencies, it ensures that all dependencies are
-automatically enabled. If any of the dependencies are built as MODULE libraries,
-the extension must also be built as a MODULE library.
+PHP extension dependencies can be specified with the following target properties:
+* PHP_REQUIRED_EXTENSIONS
+* PHP_OPTIONAL_EXTENSIONS
+* PHP_RECOMMENDED_EXTENSIONS
+* PHP_CONFLICTING_EXTENSIONS
 
-Dependencies can be specified on top of the CMake's built-in command
-add_dependencies(), which builds target dependencies before the target itself.
-Commands defined here read the add_dependencies() invocations in extensions
-CMakeLists.txt files and automatically enable and configure them as MODULE
-depending on the configuration if they haven't been explicitly configured. If
-extension dependencies can't be automatically configured it will result in a
-fatal error at the end of the configuration phase.
+If any of the required dependencies are built as MODULE libraries, the extension
+must also be built as a MODULE library.
 
 The order of the extensions is important in the generated
 'main/internal_functions*.c' files (for the list of 'phpext_<extension>_ptr' in
 the 'zend_module_entry php_builtin_extensions'). This is the order of how the
 PHP modules are registered into the Zend hash table.
 
-PHP internal API also provides dependencies handling with the
-'ZEND_MOD_REQUIRED', 'ZEND_MOD_CONFLICTS', and 'ZEND_MOD_OPTIONAL', which should
-be set in the extension code itself. PHP internally then sorts the extensions
-based on the 'ZEND_MOD_REQUIRED' and 'ZEND_MOD_OPTIONAL', so build time sorting
-shouldn't be taken for granted and is mostly used for php-src builds.
+PHP core also provides dependencies handling with the 'ZEND_MOD_REQUIRED',
+'ZEND_MOD_OPTIONAL', and 'ZEND_MOD_CONFLICTS', which should be set in the
+extension source code. PHP internally then sorts the extensions based on the
+'ZEND_MOD_REQUIRED' and 'ZEND_MOD_OPTIONAL'.
 
 Example why setting dependencies with 'ZEND_MOD_REQUIRED' might matter:
 https://bugs.php.net/53141
@@ -36,66 +31,8 @@ https://bugs.php.net/53141
 
 include_guard(GLOBAL)
 
-# Sorts extensions by dependencies and evaluate their configuration options.
-function(php_extensions_preprocess)
-  set(result ${ARGV0})
-  set(extensions ${${ARGV0}})
-
-  _php_extensions_sort(extensions)
-  _php_extensions_eval_options("${extensions}")
-
-  foreach(extension IN LISTS extensions)
-    string(TOUPPER "${extension}" extension_upper)
-
-    if(NOT PHP_EXT_${extension_upper})
-      continue()
-    endif()
-
-    # Mark shared option variable as advanced.
-    if(DEFINED PHP_EXT_${extension_upper}_SHARED)
-      mark_as_advanced(PHP_EXT_${extension_upper}_SHARED)
-    endif()
-
-    _php_extensions_get_dependencies("${extension}" dependencies)
-
-    # If extension is enabled and one of its dependencies is built as a shared
-    # library, configure extension also as a shared library.
-    foreach(dependency IN LISTS dependencies)
-      string(TOUPPER "${dependency}" dependency_upper)
-
-      if(
-        PHP_EXT_${dependency_upper}_SHARED
-        AND NOT PHP_EXT_${extension_upper}_SHARED
-      )
-        message(
-          WARNING
-          "The '${extension}' extension must be built as a shared library as "
-          "its dependency '${dependency}' extension is configured as shared. "
-          "The 'PHP_EXT_${extension_upper}_SHARED' option has been "
-          "automatically set to 'ON'."
-        )
-
-        set(
-          CACHE{PHP_EXT_${extension_upper}_SHARED}
-          TYPE BOOL
-          HELP "Build the ${extension} extension as a shared library"
-          FORCE
-          VALUE ON
-        )
-
-        break()
-      endif()
-    endforeach()
-  endforeach()
-
-  # Validate extensions and their dependencies after extensions are configured.
-  cmake_language(DEFER CALL _php_extensions_validate)
-
-  set(${result} ${${result}} PARENT_SCOPE)
-endfunction()
-
 # Sorts extensions by their dependencies.
-function(_php_extensions_sort)
+function(php_extensions_sort)
   cmake_parse_arguments(
     PARSE_ARGV
     1
@@ -109,22 +46,42 @@ function(_php_extensions_sort)
     message(FATAL_ERROR "Unrecognized arguments: ${parsed_UNPARSED_ARGUMENTS}")
   endif()
 
-  set(result ${ARGV0})
-  set(extensions ${${ARGV0}})
   set(extensions_before "")
   set(extensions_middle "")
 
-  foreach(extension IN LISTS extensions)
-    _php_extensions_parse_dependencies("${extension}" dependencies)
-    set_property(
-      GLOBAL
-      APPEND
-      PROPERTY
-        PHP_EXTENSION_${extension}_DEPS ${dependencies}
+  foreach(extension IN LISTS ${ARGV0})
+    get_target_property(
+      dependencies
+      PHP::ext::${extension}
+      PHP_REQUIRED_EXTENSIONS
     )
+
+    get_target_property(
+      optional_dependencies
+      PHP::ext::${extension}
+      PHP_OPTIONAL_EXTENSIONS
+    )
+
+    if(optional_dependencies)
+      list(APPEND dependencies "${optional_dependencies}")
+    endif()
+
+    get_target_property(
+      recommended_extensions
+      PHP::ext::${extension}
+      PHP_RECOMMENDED_EXTENSIONS
+    )
+
+    if(recommended_extensions)
+      list(APPEND dependencies "${recommended_extensions}")
+    endif()
 
     if(dependencies)
       foreach(dependency IN LISTS dependencies)
+        if(NOT TARGET PHP::ext::${dependency})
+          continue()
+        endif()
+
         list(REMOVE_ITEM extensions_middle ${dependency})
 
         if(NOT dependency IN_LIST extensions_before)
@@ -139,314 +96,8 @@ function(_php_extensions_sort)
     endif()
   endforeach()
 
-  list(APPEND extensions_sorted ${extensions_before} ${extensions_middle})
-  list(REMOVE_DUPLICATES extensions_sorted)
-  list(REVERSE extensions_sorted)
+  set(${ARGV0} ${extensions_before} ${extensions_middle})
+  list(REMOVE_DUPLICATES ${ARGV0})
 
-  set(${result} ${extensions_sorted} PARENT_SCOPE)
-endfunction()
-
-# Gets extension dependencies from the add_dependencies().
-function(_php_extensions_parse_dependencies extension result)
-  unset(${result} PARENT_SCOPE)
-
-  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${extension}/CMakeLists.txt)
-    return()
-  endif()
-
-  file(READ ${CMAKE_CURRENT_SOURCE_DIR}/${extension}/CMakeLists.txt content)
-
-  _php_extensions_remove_comments(content)
-
-  string(CONCAT regex
-    # Command invocation:
-    "add_dependencies[ \t]*\\("
-    # Target name:
-    "[ \t\n]*php_ext_${extension}[ \t\n]+"
-    # Dependencies:
-    "[\"]?(php_ext_[a-zA-Z0-9_; \t\n]+)"
-  )
-
-  string(REGEX MATCHALL "${regex}" matches "${content}")
-
-  set(all_dependencies "")
-
-  foreach(match IN LISTS matches)
-    if(match MATCHES "${regex}")
-      if(CMAKE_MATCH_1)
-        string(STRIP "${CMAKE_MATCH_1}" dependencies)
-        string(REPLACE " " ";" dependencies "${dependencies}")
-        list(TRANSFORM dependencies REPLACE "^php_ext_" "")
-        list(APPEND all_dependencies ${dependencies})
-      endif()
-    endif()
-  endforeach()
-
-  if(all_dependencies)
-    list(REMOVE_DUPLICATES all_dependencies)
-
-    get_property(all_extensions GLOBAL PROPERTY PHP_ALL_EXTENSIONS)
-
-    foreach(dependency IN LISTS all_dependencies)
-      if(NOT "${dependency}" IN_LIST all_extensions)
-        list(REMOVE_ITEM all_dependencies ${dependency})
-      endif()
-    endforeach()
-
-    set(${result} "${all_dependencies}" PARENT_SCOPE)
-  endif()
-endfunction()
-
-# Gets extension dependencies if found.
-function(_php_extensions_get_dependencies extension result)
-  set(${result} "")
-
-  get_property(deps GLOBAL PROPERTY PHP_EXTENSION_${extension}_DEPS)
-  if(deps)
-    set(${result} "${deps}")
-  endif()
-
-  return(PROPAGATE ${result})
-endfunction()
-
-# Gets a regex string to match option().
-function(_php_extensions_option_regex option result)
-  string(CONCAT _
-    # Start of the option command invocation:
-    "[ \t\n]?option[ \t]*\\([ \t\n]*"
-    # Variable name:
-    "[ \t\n]*${option}[ \t\n]+"
-    # Documentation string without escaped double quotes (\"):
-    # TODO: should escaped quotes be also matched?
-    #"[ \t\n]*\"([^\"]|\\\")*\"[ \t\n]*"
-    "[ \t\n]*\"[^\"]*\"[ \t\n]*"
-    # Optional boolean or variable value:
-    "([ \t\n]+("
-    "ON|on|TRUE|true|YES|yes|Y|y|"
-    "OFF|off|FALSE|false|NO|no|N|n|"
-    "[0-9.]+|"
-    "\\\$\\{[^\\}]+\\}"
-    "))?"
-    # End of option invocation:
-    "[ \t\n]*\\)"
-  )
-
-  set(${result} "${_}" PARENT_SCOPE)
-endfunction()
-
-# Gets a regex string to match cmake_dependent_option().
-function(_php_extensions_cmake_dependent_option_regex option result)
-  string(CONCAT _
-    # Start of the option command invocation:
-    "[ \t\n]?cmake_dependent_option[ \t]*\\([ \t\n]*"
-    # Variable name:
-    "[ \t\n]*${option}[ \t\n]+"
-    # Documentation string without escaped double quotes (\"):
-    # TODO: should escaped quotes be also matched?
-    #"[ \t\n]*\"([^\"]|\\\")*\"[ \t\n]*"
-    "[ \t\n]*\"[^\"]*\"[ \t\n]*"
-    # Boolean or variable value:
-    "[ \t\n]+("
-    "ON|on|TRUE|true|YES|yes|Y|y|"
-    "OFF|off|FALSE|false|NO|no|N|n|"
-    "[0-9.]+|"
-    "\\\$\\{[^\\}]+\\}"
-    ")"
-    # Semicolon separated list of conditions:
-    "[ \t\n]*\"[^\"]*\"[ \t\n]*"
-    # Boolean or variable force value:
-    "[ \t\n]+("
-    "ON|on|TRUE|true|YES|yes|Y|y|"
-    "OFF|off|FALSE|false|NO|no|N|n|"
-    "[0-9.]+|"
-    "\\\$\\{[^\\}]+\\}"
-    ")"
-    # End of option invocation:
-    "[ \t\n]*\\)"
-  )
-
-  set(${result} "${_}" PARENT_SCOPE)
-endfunction()
-
-# Parses and evaluates options of extensions.
-function(_php_extensions_eval_options directories)
-  set(code "")
-  get_property(
-    always_enabled_extensions
-    GLOBAL
-    PROPERTY PHP_ALWAYS_ENABLED_EXTENSIONS
-  )
-  get_property(all_extensions GLOBAL PROPERTY PHP_ALL_EXTENSIONS)
-
-  foreach(extension IN LISTS extensions)
-    # Skip if extension is always enabled or if dependency is not extension.
-    if(
-      extension IN_LIST always_enabled_extensions
-      OR NOT extension IN_LIST all_extensions
-    )
-      continue()
-    endif()
-
-    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/${extension}/CMakeLists.txt content)
-    string(TOUPPER "${extension}" extension_upper)
-
-    # Check if extension has option(PHP_EXT_<extension> ...).
-    _php_extensions_remove_comments(content)
-    _php_extensions_option_regex("PHP_EXT_${extension_upper}" regex)
-    string(REGEX MATCH "${regex}" code "${content}")
-
-    if(code)
-      string(PREPEND code "set(PROJECT_IS_TOP_LEVEL FALSE)\n")
-      cmake_language(EVAL CODE "${code}")
-      unset(code)
-    endif()
-
-    # If extension has cmake_dependent_option(PHP_EXT_<extension> ...).
-    _php_extensions_cmake_dependent_option_regex(
-      "PHP_EXT_${extension_upper}"
-      regex
-    )
-    string(REGEX MATCH "${regex}" code "${content}")
-
-    if(code)
-      string(PREPEND code "set(PROJECT_IS_TOP_LEVEL FALSE)\n")
-      string(PREPEND code "include(CMakeDependentOption)\n")
-      cmake_language(EVAL CODE "${code}")
-      unset(code)
-    endif()
-
-    # If extension has cmake_dependent_option(PHP_EXT_<extension>_SHARED ...).
-    _php_extensions_cmake_dependent_option_regex(
-      "PHP_EXT_${extension_upper}_SHARED"
-      regex
-    )
-
-    string(REGEX MATCH "${regex}" code "${content}")
-
-    if(code)
-      string(PREPEND code "set(PROJECT_IS_TOP_LEVEL FALSE)\n")
-      string(PREPEND code "include(CMakeDependentOption)\n")
-      cmake_language(EVAL CODE "${code}")
-      unset(code)
-    endif()
-  endforeach()
-endfunction()
-
-# Removes line comments from CMake code content.
-function(_php_extensions_remove_comments)
-  string(REGEX REPLACE "[ \t]*#[^\n]*" "" ${ARGV0} "${${ARGV0}}")
-  set(${ARGV0} "${${ARGV0}}" PARENT_SCOPE)
-endfunction()
-
-# Postconfigures extension right after it has been configured.
-function(php_extensions_postconfigure extension)
-  if(NOT TARGET php_ext_${extension})
-    return()
-  endif()
-
-  # If extension is enabled, enable also all its dependencies.
-  get_target_property(
-    dependencies
-    php_ext_${extension}
-    MANUALLY_ADDED_DEPENDENCIES
-  )
-  list(TRANSFORM dependencies REPLACE "^php_ext_" "")
-  get_property(
-    always_enabled_extensions
-    GLOBAL
-    PROPERTY PHP_ALWAYS_ENABLED_EXTENSIONS
-  )
-  get_property(all_extensions GLOBAL PROPERTY PHP_ALL_EXTENSIONS)
-
-  foreach(dependency IN LISTS dependencies)
-    string(TOUPPER "${dependency}" dependency_upper)
-
-    if(
-      PHP_EXT_${dependency_upper}
-      OR dependency IN_LIST always_enabled_extensions
-      OR NOT dependency IN_LIST all_extensions
-    )
-      continue()
-    endif()
-
-    message(
-      WARNING
-      "The '${extension}' extension requires the '${dependency}' extension. "
-      "The 'PHP_EXT_${dependency_upper}' option has been automatically set to "
-      "'ON'."
-    )
-
-    if(DEFINED CACHE{PHP_EXT_${dependency_upper}})
-      set_property(CACHE PHP_EXT_${dependency_upper} PROPERTY VALUE ON)
-    else()
-      set(
-        CACHE{PHP_EXT_${dependency_upper}}
-        TYPE BOOL
-        HELP "Enable the ${dependency} extension"
-        FORCE
-        VALUE ON
-      )
-    endif()
-  endforeach()
-endfunction()
-
-# Validates extensions and their dependencies defined via add_dependencies().
-function(_php_extensions_validate)
-  get_directory_property(extensions SUBDIRECTORIES)
-  list(TRANSFORM extensions REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "")
-
-  foreach(extension IN LISTS extensions)
-    if(NOT TARGET php_ext_${extension})
-      continue()
-    endif()
-
-    get_target_property(
-      dependencies
-      php_ext_${extension}
-      MANUALLY_ADDED_DEPENDENCIES
-    )
-
-    if(NOT dependencies)
-      continue()
-    endif()
-
-    list(TRANSFORM dependencies REPLACE "^php_ext_" "")
-
-    get_property(all_extensions GLOBAL PROPERTY PHP_ALL_EXTENSIONS)
-
-    foreach(dependency IN LISTS dependencies)
-      # Skip dependencies that are not PHP extensions.
-      if(NOT dependency IN_LIST all_extensions)
-        continue()
-      endif()
-
-      if(NOT TARGET php_ext_${dependency} OR NOT dependency IN_LIST extensions)
-        string(TOUPPER "${dependency}" dependency_upper)
-        message(
-          SEND_ERROR
-          "You've enabled the '${extension}' extension, which depends on the "
-          "'${dependency}' extension, but you've either not enabled "
-          "'${dependency}', or have disabled it. Please set "
-          "'PHP_EXT_${dependency_upper}' to 'ON' if available."
-        )
-      endif()
-
-      get_target_property(dependency_type php_ext_${dependency} TYPE)
-      get_target_property(extension_type php_ext_${extension} TYPE)
-
-      if(
-        dependency_type STREQUAL "MODULE_LIBRARY"
-        AND NOT extension_type STREQUAL "MODULE_LIBRARY"
-      )
-        message(
-          SEND_ERROR
-          "You've configured the '${extension}' extension to be built "
-          "statically, but it depends on the '${dependency}' extension, which "
-          "you've configured to build as a shared library. You either need to "
-          "build the '${extension}' shared or build '${dependency}' statically "
-          "for the build to be successful."
-        )
-      endif()
-    endforeach()
-  endforeach()
+  return(PROPAGATE ${ARGV0})
 endfunction()
