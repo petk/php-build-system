534676d0e1073c81a9708f028101c4acf74295f1
Peter Kokot <peterkokot@gmail.com>
2026-03-27 14:22:49 +0100

    Fix and simplify PHP thread safety
    Changes:
    - PHP thread safety isn't enabled automatically anymore during Apache
      module determination. Automation at this level complicates procedural
      build configuration and makes the user unaware of it. Fatal error
      seems more appropriate here.
    - More obsolete compile definitions removed from thread-safety
      configuration - compiler on AIX sets _THREAD_SAFE automatically when
      using the -pthread flag. Similar for _REENTRANT on Solaris/illumos and
      Linux. The FreeBSD lacky bypass is left there for now due to few
      obsolete usages in php-src.
    - Added PHP thread safety result variable also to PHP config files and
      FindPHP module for building extensions as standalone.

--- cmake/cmake/modules/PHP/Core/ThreadSafety.cmake
+++ cmake/cmake/modules/PHP/Core/ThreadSafety.cmake
@@ -1,21 +1,26 @@
 #[=============================================================================[
-Check whether the PHP thread safety, a.k.a. ZTS (Zend thread safety) should be
-enabled.
+This is an internal module and is intended for usage only within the php-src.
+It checks whether the PHP thread safety, a.k.a. ZTS (Zend thread safety) should
+be enabled.
+
+Load this module in a CMake project with:
+
+  include(PHP/Core/ThreadSafety)
 
 Result variables:
 
 * ZTS - Whether PHP thread safety is enabled.
 
-Custom CMake properties:
+Including this module will also add some compile definitions to achieve POSIX
+threads conformance. These definitions are today obsolete on all modern systems.
+POSIX threading can be achieved by simply using the compiler option '-pthread',
+which is sufficient (it automatically defines the _REENTRANT and on some systems
+also _THREAD_SAFE compile definition for backward compatibility). For example,
+see the output of:
+  gcc -dumpspecs | grep _REENTRANT
+or:
+  clang -pthread -dM -E - < /dev/null | grep _REENTRANT
 
-* PHP_THREAD_SAFETY
-
-Including this internal module/file will also add some compile definitions to
-achieve POSIX threads conformance. These definitions are today obsolete on all
-modern systems. POSIX threading can be achieved by simply using the compiler
-option '-pthread', which is sufficient (it automatically defines the _REENTRANT
-and on some systems also _THREAD_SAFE compile definition for backward
-compatibility). For example, see the 'gcc -dumpspecs | grep _REENTRANT' output.
 System headers in most cases don't utilize these definitions anymore. However,
 php-src code at the time of writing, still uses the _REENTRANT and _THREAD_SAFE
 macros on few places. Therefore they are left here to not change the PHP's C
@@ -39,19 +44,13 @@ code behavior yet.
   compile option, which automatically defines the _THREAD_SAFE for backwards
   compatibility.
 
-* Similar can be concluded for other configured systems below: Linux, SCO_SV,
-  UNIX_SV, and UnixWare.
+* Similar can be concluded for other configured systems: Linux, SCO_SV, UNIX_SV,
+  and UnixWare.
 #]=============================================================================]
 
 include_guard(GLOBAL)
 
 include(FeatureSummary)
-
-define_property(
-  TARGET
-  PROPERTY PHP_THREAD_SAFETY
-  BRIEF_DOCS "Whether the PHP has thread safety enabled"
-)
 
 message(CHECK_START "Checking whether to enable thread safety (ZTS)")
 
@@ -89,15 +88,9 @@ set(ZTS TRUE)
 # directly available. For example, some Zend headers.
 target_compile_definitions(php_config INTERFACE ZTS)
 
-# Set custom target property on the PHP configuration target.
-set_target_properties(php_config PROPERTIES PHP_THREAD_SAFETY ON)
-
 # Add deprecated and obsolete compile definitions for POSIX threads conformance.
 target_compile_definitions(
   php_config
   INTERFACE
-    $<$<AND:$<PLATFORM_ID:SunOS>,$<COMPILE_LANGUAGE:ASM,C,CXX>>:_REENTRANT>
     $<$<AND:$<PLATFORM_ID:FreeBSD>,$<COMPILE_LANGUAGE:ASM,C,CXX>>:_REENTRANT;_THREAD_SAFE>
-    $<$<AND:$<PLATFORM_ID:AIX>,$<COMPILE_LANGUAGE:ASM,C,CXX>>:_THREAD_SAFE>
-    $<$<AND:$<PLATFORM_ID:Linux,SCO_SV,UNIX_SV,UnixWare>,$<COMPILE_LANGUAGE:ASM,C,CXX>>:_REENTRANT>
 )
