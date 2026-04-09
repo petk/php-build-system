5d5685492184326d6b79cdf6791a136d4a71b16e
Peter Kokot <peterkokot@gmail.com>
2026-04-09 13:57:59 +0200

    Update ext/snmp
    - Updated snmp config header comments.
    - Find module enhanced with propagating only result variables.
    - Removed over-paranoid init_snmp() check from the FindNetSnmp module.
      These kind of checks are overdoing things relevant for the find
      module and neither is init_snmp something relevant to check here
      anymore.

--- cmake/cmake/modules/FindNetSnmp.cmake
+++ cmake/cmake/modules/FindNetSnmp.cmake
@@ -11,7 +11,8 @@ find_package(NetSnmp [<version>] [...])
 
 This module provides the following imported targets:
 
-* `NetSnmp::NetSnmp` - The package library, if found.
+* `NetSnmp::NetSnmp` - Target encapsulating the package usage requirements,
+  available if package was found.
 
 ## Result variables
 
@@ -48,8 +49,6 @@ target_link_libraries(example PRIVATE NetSnmp::NetSnmp)
 ```
 #]=============================================================================]
 
-include(CheckSymbolExists)
-include(CMakePushCheckState)
 include(FeatureSummary)
 include(FindPackageHandleStandardArgs)
 
@@ -60,82 +59,90 @@ set_package_properties(
     DESCRIPTION "Simple network management protocol library"
 )
 
-set(_reason "")
+block(
+  PROPAGATE
+    NetSnmp_FOUND
+    NetSnmp_VERSION
+)
+  set(reason "")
 
-find_package(PkgConfig QUIET)
-if(PkgConfig_FOUND)
-  if(
-    NetSnmp_ROOT
-    OR NETSNMP_ROOT
-    OR DEFINED ENV{NetSnmp_ROOT}
-    OR DEFINED ENV{NETSNMP_ROOT}
-  )
-    set(saved "${CMAKE_PREFIX_PATH}")
-    list(
-      PREPEND
-      CMAKE_PREFIX_PATH
-      ${NetSnmp_ROOT}
-      ${NETSNMP_ROOT}
-      $ENV{NetSnmp_ROOT}
-      $ENV{NETSNMP_ROOT}
+  find_package(PkgConfig QUIET)
+  if(PkgConfig_FOUND)
+    if(
+      NetSnmp_ROOT
+      OR NETSNMP_ROOT
+      OR DEFINED ENV{NetSnmp_ROOT}
+      OR DEFINED ENV{NETSNMP_ROOT}
     )
+      set(saved "${CMAKE_PREFIX_PATH}")
+      list(
+        PREPEND
+        CMAKE_PREFIX_PATH
+        ${NetSnmp_ROOT}
+        ${NETSNMP_ROOT}
+        $ENV{NetSnmp_ROOT}
+        $ENV{NETSNMP_ROOT}
+      )
+    endif()
+
+    pkg_check_modules(PC_NetSnmp QUIET netsnmp)
+
+    if(saved)
+      set(CMAKE_PREFIX_PATH ${saved})
+    endif()
   endif()
 
-  pkg_check_modules(PC_NetSnmp QUIET netsnmp)
-
-  unset(CMAKE_PREFIX_PATH)
-  if(saved)
-    set(CMAKE_PREFIX_PATH ${saved})
-  endif()
-  unset(saved)
-endif()
-
-find_program(
-  NetSnmp_CONFIG_EXECUTABLE
-  NAMES net-snmp-config
-  DOC "Path to net-snmp-config utility"
-)
-mark_as_advanced(NetSnmp_CONFIG_EXECUTABLE)
-
-if(NetSnmp_CONFIG_EXECUTABLE AND NOT PC_NetSnmp_FOUND)
-  execute_process(
-    COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --prefix
-    OUTPUT_VARIABLE _netsnmp_config_include_dir
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET
+  find_program(
+    NetSnmp_CONFIG_EXECUTABLE
+    NAMES net-snmp-config
+    DOC "Path to net-snmp-config utility"
   )
+  mark_as_advanced(NetSnmp_CONFIG_EXECUTABLE)
 
-  if(_netsnmp_config_include_dir)
-    string(APPEND _netsnmp_config_include_dir "/include")
+  if(NetSnmp_CONFIG_EXECUTABLE AND NOT PC_NetSnmp_FOUND)
+    execute_process(
+      COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --prefix
+      OUTPUT_VARIABLE netsnmp_config_include_dir
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+
+    if(netsnmp_config_include_dir)
+      string(APPEND netsnmp_config_include_dir "/include")
+    endif()
+
+    execute_process(
+      COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --libdir
+      OUTPUT_VARIABLE netsnmp_config_libdir
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+
+    if(netsnmp_config_libdir)
+      string(
+        REGEX REPLACE
+        "^-L"
+        ""
+        netsnmp_config_libdir
+        ${netsnmp_config_libdir}
+      )
+    endif()
   endif()
 
-  execute_process(
-    COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --libdir
-    OUTPUT_VARIABLE _netsnmp_config_libdir
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET
+  find_path(
+    NetSnmp_INCLUDE_DIR
+    NAMES net-snmp/net-snmp-config.h
+    HINTS
+      ${PC_NetSnmp_INCLUDE_DIRS}
+      ${netsnmp_config_include_dir}
+    DOC "Directory containing Net-SNMP library headers"
   )
+  mark_as_advanced(NetSnmp_INCLUDE_DIR)
 
-  if(_netsnmp_config_libdir)
-    string(REGEX REPLACE "^-L" "" _netsnmp_config_libdir ${_netsnmp_config_libdir})
+  if(NOT NetSnmp_INCLUDE_DIR)
+    string(APPEND reason "net-snmp/net-snmp-config.h not found. ")
   endif()
-endif()
 
-find_path(
-  NetSnmp_INCLUDE_DIR
-  NAMES net-snmp/net-snmp-config.h
-  HINTS
-    ${PC_NetSnmp_INCLUDE_DIRS}
-    ${_netsnmp_config_include_dir}
-  DOC "Directory containing Net-SNMP library headers"
-)
-mark_as_advanced(NetSnmp_INCLUDE_DIR)
-
-if(NOT NetSnmp_INCLUDE_DIR)
-  string(APPEND _reason "net-snmp/net-snmp-config.h not found. ")
-endif()
-
-block()
   # Support preference of static libs by adjusting CMAKE_FIND_LIBRARY_SUFFIXES.
   if(NetSnmp_USE_STATIC_LIBS)
     if(WIN32)
@@ -150,33 +157,35 @@ block()
     NAMES netsnmp
     HINTS
       ${PC_NetSnmp_LIBRARY_DIRS}
-      ${_netsnmp_config_libdir}
+      ${netsnmp_config_libdir}
     DOC "The path to the Net-SNMP library"
   )
   mark_as_advanced(NetSnmp_LIBRARY)
-endblock()
 
-if(NOT NetSnmp_LIBRARY)
-  string(APPEND _reason "Net-SNMP library not found. ")
-endif()
+  if(NOT NetSnmp_LIBRARY)
+    string(APPEND reason "Net-SNMP library not found. ")
+  endif()
 
-# Ensure NetSnmp_CONFIG_EXECUTABLE belongs to the found Net-SNMP package.
-block()
-  if(NetSnmp_CONFIG_EXECUTABLE AND NetSnmp_LIBRARY AND _netsnmp_config_libdir)
+  # Ensure NetSnmp_CONFIG_EXECUTABLE belongs to the found Net-SNMP package.
+  if(NetSnmp_CONFIG_EXECUTABLE AND NetSnmp_LIBRARY AND netsnmp_config_libdir)
     cmake_path(GET NetSnmp_LIBRARY PARENT_PATH parent)
-    if(NOT _netsnmp_config_libdir PATH_EQUAL parent)
+    if(NOT netsnmp_config_libdir PATH_EQUAL parent)
       set_property(
         CACHE NetSnmp_CONFIG_EXECUTABLE
         PROPERTY VALUE "NetSnmp_CONFIG_EXECUTABLE-NOTFOUND"
       )
     endif()
   endif()
-endblock()
 
-# Get version.
-block(PROPAGATE NetSnmp_VERSION)
+  ##############################################################################
+  # Get version.
+  ##############################################################################
+
   if(NetSnmp_INCLUDE_DIR)
-    set(regex "^[ \t]*#[ \t]*define[ \t]+PACKAGE_VERSION[ \t]+\"([^\"]+)\"[^\n]*$")
+    set(
+      regex
+      "^[ \t]*#[ \t]*define[ \t]+PACKAGE_VERSION[ \t]+\"([^\"]+)\"[^\n]*$"
+    )
 
     file(
       STRINGS
@@ -208,90 +217,65 @@ block(PROPAGATE NetSnmp_VERSION)
       ERROR_QUIET
     )
   endif()
-endblock()
 
-# Sanity check.
-if(NetSnmp_INCLUDE_DIR AND NetSnmp_LIBRARY)
-  cmake_push_check_state(RESET)
-    set(CMAKE_REQUIRED_INCLUDES ${NetSnmp_INCLUDE_DIR})
-    set(CMAKE_REQUIRED_LIBRARIES ${NetSnmp_LIBRARY})
-    set(CMAKE_REQUIRED_QUIET TRUE)
+  ##############################################################################
 
-    block()
-      if(NetSnmp_USE_STATIC_LIBS)
-        set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
-      endif()
-      check_symbol_exists(
-        init_snmp
-        "net-snmp/net-snmp-config.h;net-snmp/net-snmp-includes.h"
-        NetSnmp_SANITY_CHECK
-      )
-    endblock()
-  cmake_pop_check_state()
-
-  if(NOT NetSnmp_SANITY_CHECK)
-    string(APPEND _reason "Sanity check failed: init_snmp not found. ")
-  endif()
-endif()
-
-find_package_handle_standard_args(
-  NetSnmp
-  REQUIRED_VARS
-    NetSnmp_LIBRARY
-    NetSnmp_INCLUDE_DIR
-    NetSnmp_SANITY_CHECK
-  VERSION_VAR NetSnmp_VERSION
-  HANDLE_VERSION_RANGE
-  REASON_FAILURE_MESSAGE "${_reason}"
-)
-
-unset(_netsnmp_config_include_dir)
-unset(_netsnmp_config_libdir)
-unset(_reason)
-
-if(NOT NetSnmp_FOUND)
-  return()
-endif()
-
-if(NOT TARGET NetSnmp::NetSnmp)
-  add_library(NetSnmp::NetSnmp UNKNOWN IMPORTED)
-
-  set_target_properties(
-    NetSnmp::NetSnmp
-    PROPERTIES
-      IMPORTED_LOCATION "${NetSnmp_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${NetSnmp_INCLUDE_DIR}"
+  find_package_handle_standard_args(
+    NetSnmp
+    REQUIRED_VARS
+      NetSnmp_LIBRARY
+      NetSnmp_INCLUDE_DIR
+    VERSION_VAR NetSnmp_VERSION
+    HANDLE_VERSION_RANGE
+    REASON_FAILURE_MESSAGE "${reason}"
   )
 
-  if(NetSnmp_USE_STATIC_LIBS)
-    set(_netsnmp_external_libraries "")
-
-    if(PC_NetSnmp_FOUND AND PC_NetSnmp_STATIC_LIBRARIES)
-      set(_netsnmp_external_libraries "${PC_NetSnmp_STATIC_LIBRARIES}")
-      list(REMOVE_ITEM _netsnmp_external_libraries netsnmp)
-    elseif(NetSnmp_CONFIG_EXECUTABLE)
-      execute_process(
-        COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --external-libs
-        OUTPUT_VARIABLE _netsnmp_external_libraries
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-      )
-      separate_arguments(
-        _netsnmp_external_libraries
-        NATIVE_COMMAND
-        "${_netsnmp_external_libraries}"
-      )
-      list(FILTER _netsnmp_external_libraries INCLUDE REGEX "^-l")
-      list(TRANSFORM _netsnmp_external_libraries REPLACE "^-l" "")
-    endif()
-
-    if(_netsnmp_external_libraries)
-      set_target_properties(
-        NetSnmp::NetSnmp
-        PROPERTIES
-          INTERFACE_LINK_LIBRARIES "${_netsnmp_external_libraries}")
-    endif()
-
-    unset(_netsnmp_external_libraries)
+  if(NOT NetSnmp_FOUND)
+    return()
   endif()
-endif()
+
+  ##############################################################################
+  # Create imported target.
+  ##############################################################################
+
+  if(NOT TARGET NetSnmp::NetSnmp)
+    add_library(NetSnmp::NetSnmp UNKNOWN IMPORTED)
+
+    set_target_properties(
+      NetSnmp::NetSnmp
+      PROPERTIES
+        IMPORTED_LOCATION "${NetSnmp_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${NetSnmp_INCLUDE_DIR}"
+    )
+
+    if(NetSnmp_USE_STATIC_LIBS)
+      set(netsnmp_external_libraries "")
+
+      if(PC_NetSnmp_FOUND AND PC_NetSnmp_STATIC_LIBRARIES)
+        set(netsnmp_external_libraries "${PC_NetSnmp_STATIC_LIBRARIES}")
+        list(REMOVE_ITEM netsnmp_external_libraries netsnmp)
+      elseif(NetSnmp_CONFIG_EXECUTABLE)
+        execute_process(
+          COMMAND "${NetSnmp_CONFIG_EXECUTABLE}" --external-libs
+          OUTPUT_VARIABLE netsnmp_external_libraries
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+          ERROR_QUIET
+        )
+        separate_arguments(
+          netsnmp_external_libraries
+          NATIVE_COMMAND
+          "${netsnmp_external_libraries}"
+        )
+        list(FILTER netsnmp_external_libraries INCLUDE REGEX "^-l")
+        list(TRANSFORM netsnmp_external_libraries REPLACE "^-l" "")
+      endif()
+
+      if(netsnmp_external_libraries)
+        set_target_properties(
+          NetSnmp::NetSnmp
+          PROPERTIES
+            INTERFACE_LINK_LIBRARIES "${netsnmp_external_libraries}")
+      endif()
+    endif()
+  endif()
+endblock()
